b1a2a349468bd96bf62790d983c897c8c7ded617
Zach Hodges <zach.hodges@nimbleapproach.com>
2025-01-09 17:54:30 +0000

    additional test working

--- src/Tests/Dfe.Complete.Application.Tests/CommandHandlers/Project/CreateProjectCommandHandlerTests.cs
+++ src/Tests/Dfe.Complete.Application.Tests/CommandHandlers/Project/CreateProjectCommandHandlerTests.cs
@@ -6,6 +6,7 @@ using Dfe.Complete.Application.Projects.Commands.CreateProject;
 using Dfe.Complete.Domain.Entities;
 using Dfe.Complete.Domain.Enums;
 using Dfe.Complete.Domain.ValueObjects;
+using Dfe.Complete.Tests.Common.Customizations.Models;
 using Dfe.Complete.Utils;
 using DfE.CoreLibs.Testing.AutoFixture.Customizations;
 
@@ -61,5 +62,53 @@ namespace Dfe.Complete.Application.Tests.CommandHandlers.Project
             await mockProjectRepository.Received(1)
                 .AddAsync(Arg.Is<Domain.Entities.Project>(s => s.Urn == command.Urn), default);
         }
+
+        [Theory]
+        [CustomAutoData(typeof(DateOnlyCustomization))]
+        public async Task Handle_ShouldSetTeamToRCcs_WhenHandOverToRcsIsTrue(
+            [Frozen] ICompleteRepository<Domain.Entities.Project> mockProjectRepository,
+            CreateConversionProjectCommandHandler handler,
+            CreateConversionProjectCommand command
+        )
+        {
+            mockProjectRepository.GetUserByAdId(Arg.Any<string>(), Arg.Any<CancellationToken>()).Returns(
+                new User { Id = new UserId(Guid.NewGuid()), Team = ProjectTeam.WestMidlands.ToDescription() });
+            
+            var now = DateTime.UtcNow;
+            var project = Domain.Entities.Project.CreateConversionProject(
+                new ProjectId(Guid.NewGuid()),
+                new Urn(2),
+                now,
+                now,
+                Domain.Enums.TaskType.Conversion,
+                Domain.Enums.ProjectType.Conversion,
+                Guid.NewGuid(),
+                DateOnly.MinValue,
+                true,
+                new Domain.ValueObjects.Ukprn(2),
+                "region",
+                true,
+                true,
+                DateOnly.MinValue,
+                "",
+                "",
+                "",
+                Guid.Empty,
+                "",
+                null,
+                null,
+                null);
+            
+            command = command with { HandingOverToRegionalCaseworkService = true };
+
+            mockProjectRepository.AddAsync(Arg.Any<Domain.Entities.Project>(), Arg.Any<CancellationToken>())
+                .Returns(Task.FromResult(project));
+            
+            await handler.Handle(command, default);
+
+            await mockProjectRepository.Received(1)
+                .AddAsync(Arg.Is<Domain.Entities.Project>(s => s.Team == "regional_casework_services"), default);
+        }
+
     }
 }
