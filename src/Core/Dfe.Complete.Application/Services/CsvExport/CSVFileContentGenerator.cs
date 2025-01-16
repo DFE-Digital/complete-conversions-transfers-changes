bcc6b38559cb35cd355e957ad49c1b5c59feeae6
Sukhvinder Bhullar <Sukhvinder.Bhullar@EDUCATION.GOV.UK>
2025-01-16 13:39:01 +0000

    CSV Generator root service added

---
+++ src/Core/Dfe.Complete.Application/Services/CsvExport/CSVFileContentGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dfe.Complete.Application.Services.CsvExport
+{
+    public interface HeaderGenerator<TModel>
+    {
+        string GenerateHeader();
+    }
+
+    public interface RowGenerator<TModel>
+    {
+        string GenerateRow(TModel model);
+    }
+
+
+    public class CSVFileContentGenerator<TModel>(HeaderGenerator<TModel> header, RowGenerator<TModel> rowGenerator)
+    {
+        public string Generate(IEnumerable<TModel> models)
+        {
+            return $"{header.GenerateHeader()}\n\r{string.Join("\n\r", models.Select(rowGenerator.GenerateRow))}";
+        }
+    }
+}
