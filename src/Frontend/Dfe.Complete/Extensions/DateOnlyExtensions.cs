1f0e906026c95b6a523259cd58f0c92147102389
Nick Warms <Nicholas.WARMS@education.gov.uk>
2025-01-17 11:49:38 +0000

    Create DateOnly Extensions

---
+++ src/Frontend/Dfe.Complete/Extensions/DateOnlyExtensions.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Dfe.Complete.Extensions
+{
+	public static class DateOnlyExtensions
+	{
+		public static string ToUkDateString(this DateOnly dateTime) => dateTime.ToString("dd/MM/yyyy");
+
+		public static string ToDateString(this DateOnly? dateTime, bool includeDayOfWeek = false)
+		{
+			if (!dateTime.HasValue)
+			{
+				return string.Empty;
+			}
+			return ToDateString(dateTime.Value, includeDayOfWeek);
+		}
+
+		public static string ToDateString(this DateOnly dateTime, bool includeDayOfWeek = false)
+		{
+			if (includeDayOfWeek)
+			{
+				return dateTime.ToString("dddd d MMMM yyyy");
+			}
+			return dateTime.ToString("d MMMM yyyy");
+		}
+
+		public static DateOnly FirstOfMonth(this DateOnly thisMonth, int monthsToAdd)
+		{
+			var month = (thisMonth.Month + monthsToAdd) % 12;
+			if (month == 0) month = 12;
+			var yearsToAdd = (thisMonth.Month + monthsToAdd - 1) / 12;
+			return new DateOnly(thisMonth.Year + yearsToAdd, month, 1);
+		}
+
+        public static string ToDateMonthYearString(this DateOnly? dateTime)
+        {
+			if (!dateTime.HasValue)
+			{
+				return string.Empty;
+			}
+
+            return dateTime.Value.ToString("MMM yyyy");
+        }
+    }
+}
