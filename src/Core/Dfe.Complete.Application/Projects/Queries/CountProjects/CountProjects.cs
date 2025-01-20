d0eb52b95b00fcd3fbce4fb8696493d0aa8cc04e
Nick Warms <Nicholas.WARMS@education.gov.uk>
2025-01-20 14:49:59 +0000

    Add caching for the CountProjects and ListAllProjects Command

--- src/Core/Dfe.Complete.Application/Projects/Queries/CountProjects/CountProjects.cs
+++ src/Core/Dfe.Complete.Application/Projects/Queries/CountProjects/CountProjects.cs
@@ -1,28 +1,45 @@
 ﻿using Dfe.Complete.Application.Common.Models;
 using Dfe.Complete.Application.Projects.Interfaces;
 using Dfe.Complete.Domain.Enums;
+using DfE.CoreLibs.Caching.Helpers;
+using DfE.CoreLibs.Caching.Interfaces;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Dfe.Complete.Application.Projects.Queries.CountProjects
 {
-    public record CountProjectQuery(ProjectState? ProjectStatus, ProjectType? Type, bool? IncludeFormAMat) : IRequest<Result<int>>;
+    public record CountProjectQuery(ProjectState? ProjectStatus, ProjectType? Type, bool? IncludeFormAMat)
+        : IRequest<Result<int>>
+    {
+        public override string ToString()
+        {
+            return $"{ProjectStatus.ToString()}{Type.ToString()}{IncludeFormAMat.ToString()}";
+        }
+    }
 
-    public class CountProjectsQueryHandler(IListAllProjectsQueryService listAllProjectsQueryService) : IRequestHandler<CountProjectQuery, Result<int>>
+    public class CountProjectsQueryHandler(
+        IListAllProjectsQueryService listAllProjectsQueryService,
+        ICacheService<IMemoryCacheType> cacheService)
+        : IRequestHandler<CountProjectQuery, Result<int>>
     {
         public async Task<Result<int>> Handle(CountProjectQuery request, CancellationToken cancellationToken)
         {
-            // var cacheKey = $"Project_{CacheKeyHelper.GenerateHashedCacheKey(request.Urn.Value.ToString())}";
-            try
+            var cacheKey = $"Project_{CacheKeyHelper.GenerateHashedCacheKey(request.ToString())}";
+            var methodName = nameof(CountProjectsQueryHandler);
+            return await cacheService.GetOrAddAsync(cacheKey, async () =>
             {
-                var result = await listAllProjectsQueryService.ListAllProjects(request.ProjectStatus, request.Type, request.IncludeFormAMat)
-                    .CountAsync(cancellationToken);
-                return Result<int>.Success(result);
-            }
-            catch (Exception ex)
-            {
-                return Result<int>.Failure(ex.Message);
-            }
+                try
+                {
+                    var result = await listAllProjectsQueryService
+                        .ListAllProjects(request.ProjectStatus, request.Type, request.IncludeFormAMat)
+                        .CountAsync(cancellationToken);
+                    return Result<int>.Success(result);
+                }
+                catch (Exception ex)
+                {
+                    return Result<int>.Failure(ex.Message);
+                }
+            }, methodName);
         }
     }
 }
--- src/Core/Dfe.Complete.Application/Projects/Queries/ListAllProjects/ListAllProjects.cs
+++ src/Core/Dfe.Complete.Application/Projects/Queries/ListAllProjects/ListAllProjects.cs
@@ -2,39 +2,61 @@
 using Dfe.Complete.Application.Projects.Interfaces;
 using Dfe.Complete.Application.Projects.Model;
 using Dfe.Complete.Domain.Enums;
+using DfE.CoreLibs.Caching.Helpers;
+using DfE.CoreLibs.Caching.Interfaces;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Dfe.Complete.Application.Projects.Queries.ListAllProjects
 {
-    public record ListAllProjectsQuery(ProjectState? ProjectStatus, ProjectType? Type, bool? IncludeFormAMat, int Page, int Count) : IRequest<Result<List<ListAllProjectsResultModel>>>;
-
-    public class ListAllProjectsQueryHandler(IListAllProjectsQueryService listAllProjectsQueryService) : IRequestHandler<ListAllProjectsQuery, Result<List<ListAllProjectsResultModel>>>
+    public record ListAllProjectsQuery(
+        ProjectState? ProjectStatus,
+        ProjectType? Type,
+        bool? IncludeFormAMat,
+        int Page,
+        int Count) : IRequest<Result<List<ListAllProjectsResultModel>>>
     {
-        public async Task<Result<List<ListAllProjectsResultModel>>> Handle(ListAllProjectsQuery request, CancellationToken cancellationToken)
+        public override string ToString()
         {
-            // var cacheKey = $"Project_{CacheKeyHelper.GenerateHashedCacheKey(request.Urn.Value.ToString())}";
-            try
+            return $"{ProjectStatus.ToString()}{Type.ToString()}{IncludeFormAMat.ToString()}{Page}{Count}";
+        }
+    }
+
+    public class ListAllProjectsQueryHandler(
+        IListAllProjectsQueryService listAllProjectsQueryService,
+        ICacheService<IMemoryCacheType> cacheService)
+        : IRequestHandler<ListAllProjectsQuery, Result<List<ListAllProjectsResultModel>>>
+    {
+        public async Task<Result<List<ListAllProjectsResultModel>>> Handle(ListAllProjectsQuery request,
+            CancellationToken cancellationToken)
+        {
+            var cacheKey = $"ListAllProjects_{CacheKeyHelper.GenerateHashedCacheKey(request.ToString())}";
+            var methodName = nameof(ListAllProjectsQueryHandler);
+            return await cacheService.GetOrAddAsync(cacheKey, async () =>
             {
-                var result = await listAllProjectsQueryService.ListAllProjects(request.ProjectStatus, request.Type, request.IncludeFormAMat)
-                    .Skip(request.Page * request.Count).Take(request.Count)
-                    .Select(item => new ListAllProjectsResultModel(
-                        item.Establishment.Name,
-                        item.Project.Id,
-                        item.Project.Urn,
-                        item.Project.SignificantDate,
-                        item.Project.State,
-                        item.Project.Type,
-                        item.Project.IncomingTrustUkprn == null,
-                        item.Project.AssignedTo
-                    ))
-                    .ToListAsync(cancellationToken);
-                return Result<List<ListAllProjectsResultModel>>.Success(result);
-            }
-            catch (Exception ex)
-            {
-                return Result<List<ListAllProjectsResultModel>>.Failure(ex.Message);
-            }
+                try
+                {
+                    var result = await listAllProjectsQueryService
+                        .ListAllProjects(request.ProjectStatus, request.Type, request.IncludeFormAMat)
+                        .Skip(request.Page * request.Count).Take(request.Count)
+                        .Select(item => new ListAllProjectsResultModel(
+                            item.Establishment.Name,
+                            item.Project.Id,
+                            item.Project.Urn,
+                            item.Project.SignificantDate,
+                            item.Project.State,
+                            item.Project.Type,
+                            item.Project.IncomingTrustUkprn == null,
+                            item.Project.AssignedTo
+                        ))
+                        .ToListAsync(cancellationToken);
+                    return Result<List<ListAllProjectsResultModel>>.Success(result);
+                }
+                catch (Exception ex)
+                {
+                    return Result<List<ListAllProjectsResultModel>>.Failure(ex.Message);
+                }
+            }, methodName);
         }
     }
 }
