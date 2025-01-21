2d998e2ff7484b9c1245ba2f6c1f2db4665d67f7
Zach Hodges <zach.hodges@nimbleapproach.com>
2025-01-21 15:05:38 +0000

    finished handler tests

--- src/Tests/Dfe.Complete.Application.Tests/CommandHandlers/Project/CreateProjectCommandHandlerTests.cs
+++ src/Tests/Dfe.Complete.Application.Tests/CommandHandlers/Project/CreateProjectCommandHandlerTests.cs
@@ -75,190 +75,115 @@ public class CreateConversionProjectCommandHandlerTests
         Assert.Equal(command.EstablishmentSharepointLink, capturedProject.EstablishmentSharepointLink);
         Assert.Equal(command.IncomingTrustSharepointLink, capturedProject.IncomingTrustSharepointLink);
         Assert.Equal(groupId.Value, capturedProject.GroupId);
-        Assert.Equal(command.HandoverComments, capturedProject.Notes.FirstOrDefault()?.Body);
+
+        var capturedNote = capturedProject.Notes.FirstOrDefault();
+        Assert.Equal(command.HandoverComments, capturedNote?.Body);
+        Assert.Equal("handover", capturedNote?.TaskIdentifier);
     }
+    
+    [Theory]
+    [CustomAutoData(typeof(DateOnlyCustomization), typeof(ProjectCustomization), typeof(IgnoreVirtualMembersCustomisation))]
+    public async Task Handle_ShouldSetTeamToRcs_WhenHandoverToRcsTrue(
+        [Frozen] ICompleteRepository<Domain.Entities.Project> mockProjectRepository,
+        [Frozen] ICompleteRepository<ConversionTasksData> mockConversionTaskRepository,
+        [Frozen] ICompleteRepository<ProjectGroup> mockProjectGroupRepository,
+        [Frozen] ICompleteRepository<User> mockUserRepository,
+        CreateConversionProjectCommandHandler handler,
+        CreateConversionProjectCommand command
+    )
+    {
+        // Arrange
+        command = command with { HandingOverToRegionalCaseworkService = true };
 
-    //
-    // [Theory]
-    // [CustomAutoData(typeof(DateOnlyCustomization))]
-    // public async Task Handle_ShouldSetTeamToRCcs_WhenHandOverToRcsIsTrue(
-    //     [Frozen] ICompleteRepository<Domain.Entities.Project> mockProjectRepository,
-    //     CreateConversionProjectCommandHandler handler,
-    //     CreateConversionProjectCommand command
-    // )
-    // {
-    //     // Arrange
-    //     var team = ProjectTeam.WestMidlands;
-    //     var user = new User
-    //     {
-    //         Id = new UserId(Guid.NewGuid()),
-    //         Team = team.ToDescription()
-    //     };
-    //     mockProjectRepository.GetUserByAdId(Arg.Any<string>(), Arg.Any<CancellationToken>()).Returns(user);
-    //
-    //     var now = DateTime.UtcNow;
-    //     var project = CreateTestProject(team, now);
-    //     command = command with { HandingOverToRegionalCaseworkService = true };
-    //
-    //     mockProjectRepository.AddAsync(Arg.Any<Domain.Entities.Project>(), Arg.Any<CancellationToken>())
-    //         .Returns(Task.FromResult(project));
-    //
-    //     // Act
-    //     await handler.Handle(command, default);
-    //
-    //     // Assert
-    //     await mockProjectRepository.Received(1)
-    //         .AddAsync(
-    //             Arg.Is<Domain.Entities.Project>(s => s.Team == ProjectTeam.RegionalCaseWorkerServices),
-    //             default);
-    // }
-    //
-    // [Theory]
-    // [CustomAutoData(typeof(DateOnlyCustomization))]
-    // public async Task Handle_ShouldSet_Team_And_AssignedTo_And_AssignedAt_WhenHandingOverToRcsFalse(
-    //     [Frozen] ICompleteRepository<Domain.Entities.Project> mockProjectRepository,
-    //     CreateConversionProjectCommandHandler handler,
-    //     CreateConversionProjectCommand command
-    // )
-    // {
-    //     // Arrange
-    //     var team = ProjectTeam.WestMidlands;
-    //     var user = new User
-    //     {
-    //         Id = new UserId(Guid.NewGuid()),
-    //         Team = team.ToDescription()
-    //     };
-    //
-    //     mockProjectRepository.GetUserByAdId(Arg.Any<string>(), Arg.Any<CancellationToken>()).Returns(user);
-    //
-    //     var now = DateTime.UtcNow;
-    //     var project = CreateTestProject(team, now);
-    //     command = command with { HandingOverToRegionalCaseworkService = false };
-    //
-    //     mockProjectRepository.AddAsync(Arg.Any<Domain.Entities.Project>(), Arg.Any<CancellationToken>())
-    //         .Returns(Task.FromResult(project));
-    //
-    //     // Act
-    //     await handler.Handle(command, default);
-    //
-    //     // Assert
-    //     await mockProjectRepository.Received(1)
-    //         .AddAsync(
-    //             Arg.Is<Domain.Entities.Project>(p =>
-    //                 p.Team == team
-    //                 && p.AssignedToId == user.Id
-    //                 && p.AssignedAt.HasValue), default);
-    // }
-    //
-    // [Theory]
-    // [CustomAutoData(typeof(DateOnlyCustomization))]
-    // public async Task Handle_ShouldSetHandover(
-    //     [Frozen] ICompleteRepository<Domain.Entities.Project> mockProjectRepository,
-    //     CreateConversionProjectCommandHandler handler,
-    //     CreateConversionProjectCommand command
-    // )
-    // {
-    //     // Arrange
-    //     var team = ProjectTeam.WestMidlands;
-    //     var user = new User
-    //     {
-    //         Id = new UserId(Guid.NewGuid()),
-    //         Team = team.ToDescription()
-    //     };
-    //
-    //     mockProjectRepository.GetUserByAdId(Arg.Any<string>(), Arg.Any<CancellationToken>()).Returns(user);
-    //
-    //     var now = DateTime.UtcNow;
-    //     var project = CreateTestProject(team, now);
-    //     command = command with { HandingOverToRegionalCaseworkService = false };
-    //
-    //     mockProjectRepository.AddAsync(Arg.Any<Domain.Entities.Project>(), Arg.Any<CancellationToken>())
-    //         .Returns(Task.FromResult(project));
-    //
-    //     // Act
-    //     await handler.Handle(command, default);
-    //
-    //     // Assert
-    //     await mockProjectRepository.Received(1)
-    //         .AddAsync(
-    //             Arg.Is<Domain.Entities.Project>(p =>
-    //                 p.Team == team
-    //                 && p.AssignedToId == user.Id
-    //                 && p.AssignedAt.HasValue), default);
-    // }
-    //
-    // [Theory]
-    // [CustomAutoData(typeof(DateOnlyCustomization))]
-    // public async Task Handle_ShouldAddNotes_WhenHandoverComments(
-    //     [Frozen] ICompleteRepository<Domain.Entities.Project> mockProjectRepository,
-    //     CreateConversionProjectCommandHandler handler,
-    //     CreateConversionProjectCommand command
-    // )
-    // {
-    //     // Arrange
-    //     var team = ProjectTeam.WestMidlands;
-    //     var user = new User
-    //     {
-    //         Id = new UserId(Guid.NewGuid()),
-    //         Team = team.ToDescription()
-    //     };
-    //     mockProjectRepository.GetUserByAdId(Arg.Any<string>(), Arg.Any<CancellationToken>()).Returns(user);
-    //
-    //     var now = DateTime.UtcNow;
-    //     var project = CreateTestProject(team, now);
-    //     command = command with
-    //     {
-    //         HandingOverToRegionalCaseworkService = false, HandoverComments = "this is a test note"
-    //     };
-    //
-    //
-    //     mockProjectRepository.AddAsync(Arg.Any<Domain.Entities.Project>(), Arg.Any<CancellationToken>())
-    //         .Returns(Task.FromResult(project));
-    //
-    //     // Act
-    //     await handler.Handle(command, default);
-    //
-    //     // Assert
-    //     await mockProjectRepository.Received(1)
-    //         .AddAsync(Arg.Is<Domain.Entities.Project>(p => p.Notes.FirstOrDefault().Body == command.HandoverComments),
-    //             default);
-    // }
+        const ProjectTeam team = ProjectTeam.WestMidlands;
+        var user = new User
+        {
+            Id = new UserId(Guid.NewGuid()),
+            Team = team.ToDescription()
+        };
 
-    // private static Domain.Entities.Project CreateTestProject(ProjectTeam team, DateTime now)
-    // {
-    //     var projectId = new ProjectId(Guid.NewGuid());
-    //     var urn = new Urn(123456);
-    //     var tasksDataId = Guid.NewGuid();
-    //     var region = Region.NorthWest;
-    //     var rdoId = new UserId(Guid.NewGuid());
-    //     var assignedTo = new UserId(Guid.NewGuid());
-    //     var assignedAt = now;
-    //
-    //     var project = Domain.Entities.Project.CreateConversionProject(
-    //         Id: projectId,
-    //         urn: urn,
-    //         createdAt: now,
-    //         updatedAt: now,
-    //         taskType: TaskType.Conversion,
-    //         projectType: ProjectType.Conversion,
-    //         tasksDataId: tasksDataId,
-    //         significantDate: new DateOnly(2025, 01, 01),
-    //         isSignificantDateProvisional: true,
-    //         incomingTrustUkprn: new Ukprn(99999999),
-    //         region: region,
-    //         isDueTo2RI: true,
-    //         hasAcademyOrderBeenIssued: true,
-    //         advisoryBoardDate: new DateOnly(2025, 06, 15),
-    //         advisoryBoardConditions: "Some conditions",
-    //         establishmentSharepointLink: "EstablishmentLink",
-    //         incomingTrustSharepointLink: "IncomingTrustLink",
-    //         groupId: Guid.NewGuid(),
-    //         team: team,
-    //         regionalDeliveryOfficerId: rdoId,
-    //         assignedToId: assignedTo,
-    //         assignedAt: assignedAt
-    //     );
-    //
-    //     return project;
-    // }
+        var createdAt = DateTime.UtcNow;
+        var conversionTaskId = Guid.NewGuid();
+        var conversionTask = new ConversionTasksData(new TaskDataId(conversionTaskId), createdAt, createdAt);
+
+        mockUserRepository.FindAsync(Arg.Any<Expression<Func<User, bool>>>()).Returns(Task.FromResult(user));
+
+        var groupId = new ProjectGroupId(Guid.NewGuid());
+        mockProjectGroupRepository.FindAsync(Arg.Any<Expression<Func<ProjectGroup, bool>>>(), Arg.Any<CancellationToken>())
+            .Returns(Task.FromResult(new ProjectGroup { Id = groupId }));
+        
+        Domain.Entities.Project capturedProject = null!;
+        
+        mockProjectRepository.AddAsync(Arg.Do<Domain.Entities.Project>(proj => capturedProject = proj), Arg.Any<CancellationToken>())
+            .Returns(Task.FromResult(capturedProject));
+
+        mockProjectRepository.AddAsync(capturedProject, Arg.Any<CancellationToken>())
+            .Returns(Task.FromResult(capturedProject));
+        
+        mockConversionTaskRepository.AddAsync(Arg.Any<ConversionTasksData>(), Arg.Any<CancellationToken>())
+            .Returns(Task.FromResult(conversionTask));
+
+        // Act
+        var projectId = await handler.Handle(command, default);
+
+        // Assert
+        Assert.NotNull(projectId);
+        Assert.Equal(ProjectTeam.RegionalCaseWorkerServices, capturedProject.Team);
+        Assert.Null(capturedProject.AssignedAt);
+        Assert.Null(capturedProject.AssignedToId);
+    }
+    
+     
+    [Theory]
+    [CustomAutoData(typeof(DateOnlyCustomization), typeof(ProjectCustomization), typeof(IgnoreVirtualMembersCustomisation))]
+    public async Task Handle_ShouldSetTeam_AssignedAt_AssignedTo_WhenNOTHandingOverToRcs(
+        [Frozen] ICompleteRepository<Domain.Entities.Project> mockProjectRepository,
+        [Frozen] ICompleteRepository<ConversionTasksData> mockConversionTaskRepository,
+        [Frozen] ICompleteRepository<ProjectGroup> mockProjectGroupRepository,
+        [Frozen] ICompleteRepository<User> mockUserRepository,
+        CreateConversionProjectCommandHandler handler,
+        CreateConversionProjectCommand command
+    )
+    {
+        // Arrange
+        //NOT Handing over to Rcs
+        command = command with { HandingOverToRegionalCaseworkService = false };
+
+        const ProjectTeam team = ProjectTeam.WestMidlands;
+        var user = new User
+        {
+            Id = new UserId(Guid.NewGuid()),
+            Team = team.ToDescription()
+        };
+
+        var createdAt = DateTime.UtcNow;
+        var conversionTaskId = Guid.NewGuid();
+        var conversionTask = new ConversionTasksData(new TaskDataId(conversionTaskId), createdAt, createdAt);
+
+        mockUserRepository.FindAsync(Arg.Any<Expression<Func<User, bool>>>()).Returns(Task.FromResult(user));
+
+        var groupId = new ProjectGroupId(Guid.NewGuid());
+        mockProjectGroupRepository.FindAsync(Arg.Any<Expression<Func<ProjectGroup, bool>>>(), Arg.Any<CancellationToken>())
+            .Returns(Task.FromResult(new ProjectGroup { Id = groupId }));
+        
+        Domain.Entities.Project capturedProject = null!;
+        
+        mockProjectRepository.AddAsync(Arg.Do<Domain.Entities.Project>(proj => capturedProject = proj), Arg.Any<CancellationToken>())
+            .Returns(Task.FromResult(capturedProject));
+
+        mockProjectRepository.AddAsync(capturedProject, Arg.Any<CancellationToken>())
+            .Returns(Task.FromResult(capturedProject));
+        
+        mockConversionTaskRepository.AddAsync(Arg.Any<ConversionTasksData>(), Arg.Any<CancellationToken>())
+            .Returns(Task.FromResult(conversionTask));
+
+        // Act
+        var projectId = await handler.Handle(command, default);
+
+        // Assert
+        Assert.NotNull(projectId);
+        Assert.Equal(team, capturedProject.Team);
+        Assert.NotNull(capturedProject.AssignedAt);
+        Assert.NotNull(capturedProject.AssignedToId);
+    }
 }
