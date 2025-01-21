3e92a60c855917729707e6d03a3a07590f3871c4
Sukhvinder Bhullar <Sukhvinder.Bhullar@EDUCATION.GOV.UK>
2025-01-21 09:45:17 +0000

    Added ignore virtual member customization

---
+++ src/Tests/Dfe.Complete.Tests.Common/Customizations/Behaviours/IgnoreVirtualMembersCustomization.cs
@@ -0,0 +1,37 @@
+﻿using AutoFixture.Kernel;
+using AutoFixture;
+using System.Reflection;
+
+namespace Dfe.Complete.Tests.Common.Customizations.Behaviours
+{
+    public class IgnoreVirtualMembers : ISpecimenBuilder
+    {
+        public Type ReflectedType { get; }
+
+        public object Create(object request, ISpecimenContext context)
+        {
+            var pi = request as PropertyInfo;
+            if (pi != null)
+            {
+                if (ReflectedType == null ||
+                    ReflectedType == pi.ReflectedType)
+                {
+                    if (pi.GetGetMethod().IsVirtual)
+                    {
+                        return new OmitSpecimen();
+                    }
+                }
+            }
+
+            return new NoSpecimen();
+        }
+    }
+
+    public class IgnoreVirtualMembersCustomisation : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customizations.Add(new IgnoreVirtualMembers());
+        }
+    }
+}
