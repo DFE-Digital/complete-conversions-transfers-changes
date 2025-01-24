2b7796df61da06d27ee31314ac3d7aff0ddde7a1
Nick Warms <Nicholas.WARMS@education.gov.uk>
2025-01-24 16:55:37 +0000

    Create Date Format constants

---
+++ src/Frontend/Dfe.Complete/Constants/DateFormatConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Dfe.Complete.Constants;
+
+public static class DateFormatConstants
+{
+    public const string DateWithDayOfTheWeek = "dddd d MMMM yyyy";
+    public const string DateWithoutDayOfTheWeek = "d MMMM yyyy";
+    public const string MonthAndYearFormat = "MMM yyyy";
+    public const string DateUkFormat = "dd/MM/yyyy";
+}
