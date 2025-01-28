5b10960a88d5ee03bdb4ae7c63a6170dcb4b9a84
Nick Warms <Nicholas.WARMS@education.gov.uk>
2025-01-28 16:16:28 +0000

    Add Tests for error handling for count and list all project queries

--- src/Tests/Dfe.Complete.Application.Tests/QueryHandlers/Project/CountAllProjectsQueryHandlerTests.cs
+++ src/Tests/Dfe.Complete.Application.Tests/QueryHandlers/Project/CountAllProjectsQueryHandlerTests.cs
@@ -2,14 +2,12 @@ using AutoFixture.Xunit2;
 using DfE.CoreLibs.Testing.AutoFixture.Attributes;
 using NSubstitute;
 using DfE.CoreLibs.Testing.AutoFixture.Customizations;
-using DfE.CoreLibs.Caching.Interfaces;
-using Dfe.Complete.Application.Common.Models;
-using DfE.CoreLibs.Caching.Helpers;
 using Dfe.Complete.Application.Projects.Interfaces;
 using Dfe.Complete.Application.Projects.Model;
 using Dfe.Complete.Application.Projects.Queries.CountAllProjects;
 using Dfe.Complete.Tests.Common.Customizations.Models;
 using MockQueryable;
+using NSubstitute.ExceptionExtensions;
 
 namespace Dfe.Complete.Application.Tests.QueryHandlers.Project
 {
@@ -23,7 +21,6 @@ namespace Dfe.Complete.Application.Tests.QueryHandlers.Project
             typeof(DateOnlyCustomization))]
         public async Task Handle_ShouldReturnCorrectCount(
             [Frozen] IListAllProjectsQueryService mockEstablishmentQueryService,
-            [Frozen] ICacheService<IMemoryCacheType> mockCacheService,
             CountAllProjectsQueryHandler handler,
             CountAllProjectsQuery query,
             List<ListAllProjectsQueryModel> listAllProjectsQueryModels)
@@ -31,27 +28,42 @@ namespace Dfe.Complete.Application.Tests.QueryHandlers.Project
             // Arrange
             var expected = listAllProjectsQueryModels.Count;
 
-            // var cacheKey = $"Project_{CacheKeyHelper.GenerateHashedCacheKey(query.ToString())}";
-
             var mock = listAllProjectsQueryModels.BuildMock();
 
             mockEstablishmentQueryService.ListAllProjects(query.ProjectStatus, query.Type)
                 .Returns(mock);
-
-            // mockCacheService.GetOrAddAsync(cacheKey, Arg.Any<Func<Task<Result<int>>>>(), Arg.Any<string>())
-            //     .Returns(callInfo =>
-            //     {
-            //         var callback = callInfo.ArgAt<Func<Task<Result<int>>>>(1);
-            //         return callback();
-            //     });
 
             // Act
             var result = await handler.Handle(query, default);
 
             // Assert
             Assert.Equal(expected, result.Value);
+        }
+        
+        [Theory]
+        [CustomAutoData(
+            typeof(OmitCircularReferenceCustomization),
+            typeof(ListAllProjectsQueryModelCustomization),
+            typeof(DateOnlyCustomization))]
+        public async Task Handle_ShouldReturnUnsuccessful_WhenAnErrorOccurs(
+            [Frozen] IListAllProjectsQueryService mockEstablishmentQueryService,
+            CountAllProjectsQueryHandler handler)
+        {
+            // Arrange
+            var errorMessage = "This is a test";
+            
+            var query = new CountAllProjectsQuery(null, null);
 
-            // await mockCacheService.Received(1).GetOrAddAsync(cacheKey, Arg.Any<Func<Task<Result<int>>>>(), nameof(CountAllProjectsQueryHandler));
+            mockEstablishmentQueryService.ListAllProjects(query.ProjectStatus, query.Type)
+                .Throws(new Exception(errorMessage));
+
+            // Act
+            var result = await handler.Handle(query, default);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.False(result.IsSuccess);
+            Assert.Equal(errorMessage, result.Error);
         }
     }
 }
