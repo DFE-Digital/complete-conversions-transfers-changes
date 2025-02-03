f0af9f6e26c63014418fb9b00c53a1d1dd8166c3
Zach Hodges <zach.hodges@nimbleapproach.com>
2025-02-03 15:56:49 +0000

    query working

--- src/Core/Dfe.Complete.Application/Projects/Queries/ListAllProjectsByRegion/ListAllProjectsByRegion.cs
+++ src/Core/Dfe.Complete.Application/Projects/Queries/ListAllProjectsByRegion/ListAllProjectsByRegion.cs
@@ -20,25 +20,29 @@ public class ListAllProjectsByRegionQueryHandler(IListAllProjectsQueryService li
             var projectsList = await listAllProjectsQueryService
                 .ListAllProjects(request.ProjectStatus, request.Type)
                 .ToListAsync(cancellationToken);
-            
-            var projectsResultModel = projectsList
-                .GroupBy(p => p.Project.Region)
+
+            var projectsGroupedByRegion = projectsList
+                .GroupBy(p => p.Project.Region);
+
+            var projectsResultModel = projectsGroupedByRegion
                 .Select(group => new ListAllProjectsByRegionResultModel(
-                Region: (Region)group.Key, 
-                ConversionsCount: group.Count(p => p.Project?.Type == ProjectType.Conversion),
-                TransfersCount: group.Count(p => p.Project?.Type == ProjectType.Transfer),
-                Projects: group.Select(item => new Model.ListAllProjectsResultModel(
-                    item.Establishment.Name,
-                    item.Project.Id,
-                    item.Project.Urn,
-                    item.Project.SignificantDate,
-                    item.Project.State,
-                    item.Project.Type,
-                    item.Project.IncomingTrustUkprn == null,
-                    item.Project.AssignedTo != null
-                        ? $"{item.Project.AssignedTo.FirstName} {item.Project.AssignedTo.LastName}"
-                        : null)).ToList()
-            )).ToList();
+                    Region: (Region)group.Key,
+                    ConversionsCount: group.Count(item => item.Project?.Type == ProjectType.Conversion),
+                    TransfersCount: group.Count(item => item.Project?.Type == ProjectType.Transfer),
+                    Projects: group
+                        .Select(item => new Model.ListAllProjectsResultModel(
+                            item.Establishment.Name,
+                            item.Project.Id,
+                            item.Project.Urn,
+                            item.Project.SignificantDate,
+                            item.Project.State,
+                            item.Project.Type,
+                            item.Project.IncomingTrustUkprn is null,
+                            item.Project.AssignedTo is not null
+                                ? $"{item.Project.AssignedTo.FirstName} {item.Project.AssignedTo.LastName}"
+                                : null
+                        ))
+                )).ToList();
             
             return Result<List<ListAllProjectsByRegionResultModel>>.Success(projectsResultModel);
         }
