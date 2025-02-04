c315d21f10fe2f5530b2818575e9289c182ebe26
Nick Warms <Nicholas.WARMS@education.gov.uk>
2025-02-04 10:59:10 +0000

    Create an all projects view model

--- src/Frontend/Dfe.Complete/Pages/Projects/List/AllProjectsModel.cs
+++ src/Frontend/Dfe.Complete/Pages/Projects/List/AllProjectsModel.cs
@@ -1,22 +1,28 @@
-﻿using Dfe.Complete.Application.Projects.Model;
+﻿using Dfe.Complete.Application.Projects.Models;
 using Dfe.Complete.Constants;
+using Dfe.Complete.Domain.Entities;
 using Dfe.Complete.Domain.Enums;
 using Dfe.Complete.Pages.Pagination;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Dfe.Complete.Pages.Projects.List.ProjectsInProgress;
+namespace Dfe.Complete.Pages.Projects.List;
 
-public class ProjectsInProgressViewModel : PageModel
+public class AllProjectsModel : PageModel
 {
     [BindProperty(SupportsGet = true)] public int PageNumber { get; set; } = 1;
 
-    public PaginationModel Pagination { get; set; } = default!;
+    public PaginationModel? Pagination { get; set; }
     
     internal int PageSize = 20;
-
+    
     public static string GetProjectSummaryUrl(ListAllProjectsResultModel project)
     {
         return string.Format(project.ProjectType == ProjectType.Conversion ? RouteConstants.ConversionProjectTaskList : RouteConstants.TransferProjectTaskList, project.ProjectId);
+    }
+    
+    public static string GetProjectSummaryUrl(Project project)
+    {
+        return string.Format(project.Type == ProjectType.Conversion ? RouteConstants.ConversionProjectTaskList : RouteConstants.TransferProjectTaskList, project.Id);
     }
 }
