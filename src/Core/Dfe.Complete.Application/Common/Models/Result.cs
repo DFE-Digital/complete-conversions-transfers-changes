77cd363479f990083f71034f1ddc46e5d331b1ee
Nick Warms <Nicholas.WARMS@education.gov.uk>
2025-02-04 10:52:43 +0000

    Create paginated request and response

--- src/Core/Dfe.Complete.Application/Common/Models/Result.cs
+++ src/Core/Dfe.Complete.Application/Common/Models/Result.cs
@@ -6,15 +6,15 @@
         public bool IsSuccess { get; }
         public string? Error { get; }
 
-        private Result(T value, bool isSuccess, string? error)
+        protected Result(T value, bool isSuccess, string? error)
         {
             Value = value;
             IsSuccess = isSuccess;
             Error = error;
         }
 
-        public static Result<T> Success(T value) => new Result<T>(value, true, null);
-        public static Result<T> Failure(string error) => new Result<T>(default!, false, error);
+        public static Result<T> Success(T value) => new(value, true, null);
+        public static Result<T> Failure(string error) => new(default!, false, error);
     }
 
 }
