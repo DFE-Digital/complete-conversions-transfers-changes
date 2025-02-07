57390714abf798cf7f43a60e1129b66268e097aa
Zach Hodges <zach.hodges@nimbleapproach.com>
2025-02-07 17:17:10 +0000

    wip

--- src/Core/Dfe.Complete.Infrastructure/QueryServices/ListAllProjectLocalAuthoritiesQueryService.cs
+++ src/Core/Dfe.Complete.Infrastructure/QueryServices/ListAllProjectLocalAuthoritiesQueryService.cs
@@ -1,17 +1,21 @@
 using Dfe.Complete.Application.Projects.Queries.ProjectsByLocalAuthority;
 using Dfe.Complete.Domain.Enums;
 using Dfe.Complete.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dfe.Complete.Infrastructure.QueryServices;
 
 internal class ListAllProjectLocalAuthoritiesQueryService(CompleteContext context)
 {
     public IQueryable<ListAllLocalAuthorities> ListAllProjectLocalAuthorities(ProjectState? projectStatus,
-        ProjectType? projectType)
+        ProjectType? type)
     {
-        
-        
-
+        var query = context.Projects
+            .Where(project => projectStatus == null || project.State == projectStatus)
+            .Where(project => type == null || type == project.Type)
+            .Include(p => p.AssignedTo)
+            .Join(context.LocalAuthorities, project => project.local)
+            
         return null; 
     }
 }
