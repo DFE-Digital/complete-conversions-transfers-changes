09c120af5e8e1c214db821d3f344848a5535b375
Sukhvinder Bhullar <Sukhvinder.Bhullar@EDUCATION.GOV.UK>
2025-02-11 11:48:17 +0000

    Added logic for environment header

---
+++ src/Frontend/Dfe.Complete/Pages/Shared/_EnvironmentHeader.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dfe.Complete.Pages.Shared
+{
+    public class _EnvironmentHeaderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
