7f80d7a2afa2624998ce7264dd4dae1c61aba7eb
Zach Hodges <zach.hodges@nimbleapproach.com>
2025-02-20 09:48:23 +0000

    add LA ID fk to user controller tests

--- src/Tests/Dfe.Complete.Api.Tests.Integration/Controllers/UsersControllerTests.cs
+++ src/Tests/Dfe.Complete.Api.Tests.Integration/Controllers/UsersControllerTests.cs
@@ -1,10 +1,8 @@
-using System.Net;
 using AutoFixture;
 using Dfe.Complete.Api.Tests.Integration.Customizations;
 using Dfe.Complete.Client.Contracts;
 using Dfe.Complete.Domain.Entities;
 using Dfe.Complete.Infrastructure.Database;
-using Dfe.Complete.Tests.Common.Customizations.Commands;
 using Dfe.Complete.Tests.Common.Customizations.Models;
 using DfE.CoreLibs.Testing.AutoFixture.Attributes;
 using DfE.CoreLibs.Testing.Mocks.WebApplicationFactory;
@@ -38,12 +36,15 @@ public class UsersControllerTests
                 {
                     RegionalDeliveryOfficerId = testUser.Id,
                     CaseworkerId = testUser.Id,
-                    AssignedToId = testUser.Id
+                    AssignedToId = testUser.Id,
                 })
                 .Create<Project>();
             project.Urn = establishment.Urn ?? project.Urn;
             return project;
         }).ToList();
+        
+        projects.ForEach(p => p.LocalAuthorityId = dbContext.LocalAuthorities.FirstOrDefault().Id);
+        
         await dbContext.Projects.AddRangeAsync(projects);
 
         await dbContext.SaveChangesAsync();
