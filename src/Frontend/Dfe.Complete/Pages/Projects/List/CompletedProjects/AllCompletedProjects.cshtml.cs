da17df8af8ab8da956d2e49cac4832e53eeb37c7
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-03-06 15:46:15 +0000

    create completed projects page

---
+++ src/Frontend/Dfe.Complete/Pages/Projects/List/CompletedProjects/AllCompletedProjects.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Dfe.Complete.Application.Projects.Models;
+using Dfe.Complete.Application.Projects.Queries.CountAllProjects;
+using Dfe.Complete.Application.Projects.Queries.ListAllProjects;
+using Dfe.Complete.Domain.Enums;
+using Dfe.Complete.Models;
+using Dfe.Complete.Pages.Pagination;
+using MediatR;
+
+namespace Dfe.Complete.Pages.Projects.List.CompletedProjects;
+
+public class AllCompletedProjectsViewModel(ISender sender) : AllProjectsModel(CompletedNavigation)
+{
+    public List<ListAllProjectsResultModel> Projects { get; set; } = default!;
+
+    public async Task OnGet()
+    {
+        ViewData[TabNavigationModel.ViewDataKey] = AllProjectsTabNavigationModel;
+
+        var listProjectQuery = new ListAllProjectsQuery(ProjectState.Completed, null, PageNumber - 1, PageSize);
+
+        var listResponse = await sender.Send(listProjectQuery);
+        Projects = listResponse.Value ?? [];
+
+        var countProjectQuery = new CountAllProjectsQuery(ProjectState.Completed, null);
+        var countResponse = await sender.Send(countProjectQuery);
+
+        Pagination = new PaginationModel("/projects/all/completed/all", PageNumber, countResponse.Value, PageSize);
+    }
+}
