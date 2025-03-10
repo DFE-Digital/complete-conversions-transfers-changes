12d1d5044c6be9f87285b76758afd2225feee04f
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-03-10 16:48:20 +0000

    add route your teams layout

--- src/Frontend/Dfe.Complete/Constants/RouteConstants.cs
+++ src/Frontend/Dfe.Complete/Constants/RouteConstants.cs
@@ -7,6 +7,8 @@
 
         public const string ProjectsByRegion = "/projects/all/regions/{0}";
 
+        public const string TeamProjectsInProgress = "/projects/team/in-progress";
+
         // Project
         public const string Project = "/projects/{0}";
         public const string CreateNewProject = "/projects/CreateNewProject";
---
+++ src/Frontend/Dfe.Complete/Pages/Projects/List/YourTeamProjectsLayout.cs
@@ -0,0 +1,25 @@
+﻿// using Dfe.Complete.Application.Projects.Models;
+// using Dfe.Complete.Constants;
+// using Dfe.Complete.Domain.Entities;
+// using Dfe.Complete.Domain.Enums;
+using Dfe.Complete.Models;
+using Dfe.Complete.Pages.Pagination;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dfe.Complete.Pages.Projects.List;
+
+public abstract class YourTeamProjectsModel(string currentNavigation) : PageModel
+{
+    protected TabNavigationModel YourTeamProjectsTabNavigationModel = new(TabNavigationModel.YourTeamProjectsTabName);
+
+    public const string InProgressNavigation = "in-progress";
+
+    public string CurrentNavigationItem { get; init; } = currentNavigation;
+
+    [BindProperty(SupportsGet = true)] public int PageNumber { get; set; } = 1;
+
+    public PaginationModel? Pagination { get; set; }
+
+    internal int PageSize = 20;
+}
