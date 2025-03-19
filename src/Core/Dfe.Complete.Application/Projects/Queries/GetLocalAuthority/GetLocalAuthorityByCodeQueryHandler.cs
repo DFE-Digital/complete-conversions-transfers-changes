6a396893c97f499c5a5e48e0806949ba05c34e86
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-03-19 22:39:36 +0000

    add local authority query

---
+++ src/Core/Dfe.Complete.Application/Projects/Queries/GetLocalAuthority/GetLocalAuthorityByCodeQueryHandler.cs
@@ -0,0 +1,26 @@
+using Dfe.Complete.Application.Common.Models;
+using Dfe.Complete.Domain.Entities;
+using Dfe.Complete.Domain.Interfaces.Repositories;
+using Dfe.Complete.Application.Projects.Models;
+using MediatR;
+
+namespace Dfe.Complete.Application.Projects.Queries.GetLocalAuthority;
+
+public record GetLocalAuthorityByCodeQuery(string Code) : IRequest<Result<LocalAuthorityDto?>>;
+
+public class GetLocalAuthorityByCodeQueryHandler(ICompleteRepository<LocalAuthority> localAuthorityRepo)
+    : IRequestHandler<GetLocalAuthorityByCodeQuery, Result<LocalAuthorityDto?>>
+{
+    public async Task<Result<LocalAuthorityDto?>> Handle(GetLocalAuthorityByCodeQuery request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var localAuthority = await localAuthorityRepo.GetAsync(x => x.Code == request.Code);
+            return Result<LocalAuthorityDto?>.Success(LocalAuthorityDto.MapLAEntityToDto(localAuthority));
+        }
+        catch (Exception e)
+        {
+            return Result<LocalAuthorityDto?>.Failure(e.Message);
+        }
+    }
+}
