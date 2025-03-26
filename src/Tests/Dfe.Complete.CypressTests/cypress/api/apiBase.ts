73f25bf8ac584e7436597017736ad8f0fcf1480d
paddy-clark <patrick.clark@nimbleapproach.com>
2025-03-26 11:40:48 +0000

    Revert "revert to generate token for each request"
    This reverts commit 2424a082009dad230847697ec31b72556549fc18.

--- src/Tests/Dfe.Complete.CypressTests/cypress/api/apiBase.ts
+++ src/Tests/Dfe.Complete.CypressTests/cypress/api/apiBase.ts
@@ -1,15 +1,20 @@
-import { EnvCompleteApiClientId, EnvClientId, EnvClientSecret, EnvTenantId, EnvUsername } from "cypress/constants/cypressConstants";
+import { EnvCompleteApiClientId, EnvClientId, EnvClientSecret, EnvTenantId, EnvUsername, UserAccessToken } from "cypress/constants/cypressConstants";
 
 export class ApiBase {
-    protected getHeaders(accessToken: string): object {
+    protected getHeaders(): object {
         return {
-            Authorization: `Bearer ${accessToken}`,
+            Authorization: `Bearer ${Cypress.env(UserAccessToken)}`,
             "Content-type": "application/json",
             "x-user-context-name": Cypress.env(EnvUsername),
         };
     }
 
     protected authenticatedRequest(): Cypress.Chainable<object> {
+        const accessToken = Cypress.env(UserAccessToken);
+        if (accessToken) {
+            return cy.wrap(this.getHeaders());
+        }
+
         const tenantId = Cypress.env(EnvTenantId);  
         const clientId = Cypress.env(EnvClientId);
         const clientSecret = Cypress.env(EnvClientSecret);
@@ -29,8 +34,9 @@ export class ApiBase {
         }).then((response) => {
             expect(response.status).to.eq(200);
             const accessToken = response.body.access_token;
+            Cypress.env(UserAccessToken, accessToken);
 
-            return this.getHeaders(accessToken);
+            return this.getHeaders();
         })
     }
 }
