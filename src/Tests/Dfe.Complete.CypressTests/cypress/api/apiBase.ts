3e046a1f56cc645ce32e394f19f90da93f5f0a98
paddy-clark <patrick.clark@nimbleapproach.com>
2025-03-26 10:45:05 +0000

    Revert "debug hardcode (temp)"
    This reverts commit 37a38a2e6cf2d307b7a49ec6c444137efae41020.

--- src/Tests/Dfe.Complete.CypressTests/cypress/api/apiBase.ts
+++ src/Tests/Dfe.Complete.CypressTests/cypress/api/apiBase.ts
@@ -3,17 +3,17 @@ import { EnvCompleteApiClientId, EnvClientId, EnvClientSecret, EnvTenantId, EnvU
 export class ApiBase {
     protected getHeaders(): object {
         return {
-            Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IkpETmFfNGk0cjdGZ2lnTDNzSElsSTN4Vi1JVSIsImtpZCI6IkpETmFfNGk0cjdGZ2lnTDNzSElsSTN4Vi1JVSJ9.eyJhdWQiOiJhcGk6Ly8zZDBmOWRjMS1kN2QyLTRjODQtOWQxYy04NzNjNzZmZWZlZmMiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC85YzdkOWRkMy04NDBjLTRiM2YtODE4ZS01NTI4NjUwODJlMTYvIiwiaWF0IjoxNzQyOTgzNDIzLCJuYmYiOjE3NDI5ODM0MjMsImV4cCI6MTc0Mjk4NzMyMywiYWlvIjoiazJSZ1lEQ3Q5OXB1Nlc2bmNkcm8yOWQvWnpseUFBPT0iLCJhcHBpZCI6ImE2MjQ5MTk4LTJjMGUtNGRjNi05OGFiLWZmYThhYTliODRmMyIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzljN2Q5ZGQzLTg0MGMtNGIzZi04MThlLTU1Mjg2NTA4MmUxNi8iLCJvaWQiOiJlZGQwYzQzNS00ZmM4LTQyNzgtYjYwYi05YmJiMmRlNWU5NmMiLCJyaCI6IjEuQVRBQTA1MTluQXlFUDB1QmpsVW9aUWd1RnNHZER6M1MxNFJNblJ5SFBIYi1fdnhEQVFBd0FBLiIsInJvbGVzIjpbIkFQSS5EZWxldGUiLCJBUEkuUmVhZCIsIkFQSS5Xcml0ZSIsIkFQSS5VcGRhdGUiXSwic3ViIjoiZWRkMGM0MzUtNGZjOC00Mjc4LWI2MGItOWJiYjJkZTVlOTZjIiwidGlkIjoiOWM3ZDlkZDMtODQwYy00YjNmLTgxOGUtNTUyODY1MDgyZTE2IiwidXRpIjoibzJDN25GRFpuRU9TTFV0NjJNZE5BQSIsInZlciI6IjEuMCJ9.U36F9WLG0ZX6zmvcEj4M2keESR95qaHb5J-OBp5DgJqm4S69ATOsn8vwjLoV_daR6lYsExg-Suxtw7NRd_hwDVY2_1oyx2eFx66JbUWLoHRjl7rSllMOMDT-e7UFeV5s2XAwtNrnxWCYJeL3xjOZ6mxvFzFQqTLwJn9lgcRYpKzI_QJ8RBCLJIVgRDGZIHJABup0B-IWKVBXrt0GNLXZ2v-uxM2VscmdQkNBRtm2myXPaxR8uaSZ9QUyN2p4Z5R69P8rxC_DYTlzoCAXUeUSIzt_wYdC9iXDKp-SYL6fSg4lPke4tKBb3I3Scic1ZiZhmXskzuDb6iruk-63HyVt7Q',
+            Authorization: `Bearer ${Cypress.env(UserAccessToken)}`,
             "Content-type": "application/json",
             "x-user-context-name": Cypress.env(EnvUsername),
         };
     }
 
     protected authenticatedRequest(): Cypress.Chainable<object> {
-        // const accessToken = Cypress.env(UserAccessToken);
-        // if (accessToken) {
-        return cy.wrap(this.getHeaders());
-        // }
+        const accessToken = Cypress.env(UserAccessToken);
+        if (accessToken) {
+            return cy.wrap(this.getHeaders());
+        }
 
         const tenantId = Cypress.env(EnvTenantId);  
         const clientId = Cypress.env(EnvClientId);
