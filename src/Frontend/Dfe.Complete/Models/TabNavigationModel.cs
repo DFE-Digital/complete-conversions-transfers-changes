9c016c81e072785356a09dfcfde0790e9fbf311f
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-04-07 17:22:41 +0100

    add group and service support tabs

--- src/Frontend/Dfe.Complete/Models/TabNavigationModel.cs
+++ src/Frontend/Dfe.Complete/Models/TabNavigationModel.cs
@@ -1,7 +1,4 @@
-﻿using Dfe.Complete.Domain.Enums;
-using Dfe.Complete.Domain.Extensions;
-
-namespace Dfe.Complete.Models;
+﻿namespace Dfe.Complete.Models;
 
 public class TabNavigationModel(string currentTab)
 {
@@ -9,6 +6,8 @@ public class TabNavigationModel(string currentTab)
     public const string YourProjectsTabName = "your-projects";
     public const string YourTeamProjectsTabName = "your-team-projects";
     public const string AllProjectsTabName = "all-projects";
+    public const string GroupsTabName = "groups";
+    public const string ServiceSupportProjectsTabName = "service-support";
 
     public string CurrentTab { get; } = currentTab;
 }
