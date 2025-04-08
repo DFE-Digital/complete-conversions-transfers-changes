3d3243c032da28251e33c8e72aeb5169fe19d3d7
tristancollen <159437649+tristancollen@users.noreply.github.com>
2025-04-08 16:58:57 +0100

    Update IHostEnvironmentExtensions.cs

--- src/Frontend/Dfe.Complete/Extensions/IHostEnvironmentExtensions.cs
+++ src/Frontend/Dfe.Complete/Extensions/IHostEnvironmentExtensions.cs
@@ -5,8 +5,9 @@ namespace Dfe.Complete.Extensions;
 public static class IHostEnvironmentExtensions {
         public static bool IsTest(this IHostEnvironment hostEnvironment)
         {
-            if (hostEnvironment == null) throw new InvalidOperationException("no");
-
+            if (hostEnvironment == null)
+                throw new ArgumentNullException(nameof(hostEnvironment));
+                
             return hostEnvironment.IsEnvironment("Test");
         }
 }
