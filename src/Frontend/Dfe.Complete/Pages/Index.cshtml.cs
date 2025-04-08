ad1186ce64a76ae7543f58516f1edc29786789f4
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-04-08 15:09:03 +0100

    conditional root reroute for user team

--- src/Frontend/Dfe.Complete/Pages/Index.cshtml.cs
+++ src/Frontend/Dfe.Complete/Pages/Index.cshtml.cs
@@ -1,16 +1,26 @@
 ﻿using Dfe.Complete.Constants;
+using Dfe.Complete.Domain.Enums;
+using Dfe.Complete.Extensions;
 using Dfe.Complete.Models;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Dfe.Complete.Pages
 {
-    public class IndexModel : PageModel
-    {
-		public Task<IActionResult> OnGetAsync()
+	public class IndexModel : PageModel
+	{
+		public async Task<IActionResult> OnGetAsync([FromServices] ISender sender)
 		{
 			ViewData[TabNavigationModel.ViewDataKey] = new TabNavigationModel(TabNavigationModel.YourProjectsTabName);
-			return Task.FromResult<IActionResult>(Redirect(RouteConstants.YourProjectsInProgress));
+
+			var userTeam = await User.GetUserTeam(sender);
+
+			var route = userTeam is ProjectTeam.BusinessSupport or ProjectTeam.DataConsumers
+				? RouteConstants.ProjectsInProgress
+				: RouteConstants.YourProjectsInProgress;
+
+			return Redirect(route);
 		}
 	}
 }
