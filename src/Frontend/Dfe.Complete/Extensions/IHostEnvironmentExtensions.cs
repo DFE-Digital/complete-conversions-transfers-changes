2b4d4132f047fc186977fee5241a13cd56915b61
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-04-08 17:07:23 +0100

    add tests

--- src/Frontend/Dfe.Complete/Extensions/IHostEnvironmentExtensions.cs
+++ src/Frontend/Dfe.Complete/Extensions/IHostEnvironmentExtensions.cs
@@ -1,13 +1,11 @@
-using System;
-
 namespace Dfe.Complete.Extensions;
 
-public static class IHostEnvironmentExtensions {
-        public static bool IsTest(this IHostEnvironment hostEnvironment)
-        {
-            if (hostEnvironment == null)
-                throw new ArgumentNullException(nameof(hostEnvironment));
-                
-            return hostEnvironment.IsEnvironment("Test");
-        }
+public static class IHostEnvironmentExtensions
+{
+    public static bool IsTest(this IHostEnvironment hostEnvironment)
+    {
+        ArgumentNullException.ThrowIfNull(hostEnvironment);
+
+        return hostEnvironment.IsEnvironment("Test");
+    }
 }
