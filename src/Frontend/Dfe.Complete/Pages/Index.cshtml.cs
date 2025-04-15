5b430da706538d6449d94d08a031ce64102ab71e
tristancollen <159437649+tristancollen@users.noreply.github.com>
2025-04-15 12:24:08 +0100

    Feature/208988 navbar user permissions (#148)
    * restrict handover tab access
    
    * add group and service support tabs
    
    * restrict tab access according to ruby
    
    * conditional root reroute for user team
    
    * add in-progress formamat link
    
    * hide menu for some groups
    
    * update changelog

--- src/Frontend/Dfe.Complete/Pages/Index.cshtml.cs
+++ src/Frontend/Dfe.Complete/Pages/Index.cshtml.cs
@@ -1,16 +1,28 @@
 ﻿using Dfe.Complete.Constants;
+using Dfe.Complete.Domain.Enums;
+using Dfe.Complete.Extensions;
 using Dfe.Complete.Models;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Dfe.Complete.Pages
 {
-    public class IndexModel : PageModel
-    {
-		public Task<IActionResult> OnGetAsync()
+	public class IndexModel : PageModel
+	{
+		public async Task<IActionResult> OnGetAsync([FromServices] ISender sender)
 		{
 			ViewData[TabNavigationModel.ViewDataKey] = new TabNavigationModel(TabNavigationModel.YourProjectsTabName);
-			return Task.FromResult<IActionResult>(Redirect(RouteConstants.YourProjectsInProgress));
+
+			var userTeam = await User.GetUserTeam(sender);
+
+			string route;
+
+			if (userTeam is ProjectTeam.BusinessSupport or ProjectTeam.DataConsumers) route = RouteConstants.ProjectsInProgress;
+			else if (userTeam is ProjectTeam.ServiceSupport) route = RouteConstants.ServiceSupportProjects;
+			else route = RouteConstants.YourProjectsInProgress;
+
+			return Redirect(route);
 		}
 	}
 }
