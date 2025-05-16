cf119e4326c9c342c6398ecf3760b8984c52d201
SHAKIR, Muhammad <Muhammad.SHAKIR@EDUCATION.GOV.UK>
2025-05-16 16:52:09 +0100

    Set it up service support section

---
+++ src/Frontend/Dfe.Complete/Pages/Projects/ServiceSupport/ServiceSupportModel.cs
@@ -0,0 +1,15 @@
+﻿using Dfe.Complete.Models;
+
+namespace Dfe.Complete.Pages.Projects.ServiceSupport
+{
+    public class ServiceSupportModel(string currentSubNavigationItem) : BaseProjectsPageModel(currentSubNavigationItem)
+    {
+
+        protected TabNavigationModel AllProjectsTabNavigationModel = new(TabNavigationModel.AllProjectsTabName);
+
+        public const string ConversionURNsNavigation = "conversion-urns";
+        public const string LocalAuthoriesNavigation = "local-authorites";
+
+        public string CurrentSubNavigationItem { get; set; } = currentSubNavigationItem;
+    }
+}
