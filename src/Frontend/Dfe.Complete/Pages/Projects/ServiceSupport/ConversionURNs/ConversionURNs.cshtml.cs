d27974b065370f7689cf25e9d84b949b3fec2ad5
mshakirdfe <muhammad.shakir@education.gov.uk>
2025-05-19 11:44:46 +0100

    Set it up service support section (#230)
    * Set it up service support section

---
+++ src/Frontend/Dfe.Complete/Pages/Projects/ServiceSupport/ConversionURNs/ConversionURNs.cshtml.cs
@@ -0,0 +1,11 @@
+using MediatR;
+
+namespace Dfe.Complete.Pages.Projects.ServiceSupport.ConversionURNs
+{
+    public class ConversionURNsModel() : ServiceSupportModel(ConversionURNsNavigation)
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/Frontend/Dfe.Complete/Pages/Projects/ServiceSupport/LocalAuthorities/LocalAuthorities.cshtml.cs
@@ -0,0 +1,9 @@
+namespace Dfe.Complete.Pages.Projects.ServiceSupport.LocalAuthorities
+{
+    public class LocalAuthoritiesModel() : ServiceSupportModel(LocalAuthoriesNavigation)
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
