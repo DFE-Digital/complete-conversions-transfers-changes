1b9ff9f98c31de75a3d566e8d22a8875d491773d
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-05-20 14:43:59 +0100

    add route auth tests

--- src/Frontend/Dfe.Complete/Pages/Projects/CreateNewProject.cshtml.cs
+++ src/Frontend/Dfe.Complete/Pages/Projects/CreateNewProject.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Dfe.Complete.Pages.Projects
 {
+    [Authorize(policy: "CanCreateProjects")]
     public class CreateNewProjectModel : PageModel
     {
         [BindProperty]
