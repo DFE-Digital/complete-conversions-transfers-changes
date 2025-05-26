6cf48c7a160ba618064cff9fb34a2666a1761b3c
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-05-26 17:35:14 +0100

    optimise query

--- src/Core/Dfe.Complete.Application/Projects/Queries/ListAllProjects/ListAllProjectByLocalAuthorities.cs
+++ src/Core/Dfe.Complete.Application/Projects/Queries/ListAllProjects/ListAllProjectByLocalAuthorities.cs
@@ -4,6 +4,7 @@ using Dfe.Complete.Application.Projects.Models;
 using Dfe.Complete.Domain.Entities;
 using Dfe.Complete.Domain.Enums;
 using Dfe.Complete.Domain.Interfaces.Repositories;
+using Dfe.Complete.Utils;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -13,39 +14,38 @@ namespace Dfe.Complete.Application.Projects.Queries.ListAllProjects;
 public record ListAllProjectsByLocalAuthoritiesQuery(ProjectState? State = ProjectState.Active, ProjectType? Type = null)
     : PaginatedRequest<PaginatedResult<List<ListAllProjectLocalAuthoritiesResultModel>>>;
 
-public class ListAllProjectByLocalAuthorities(ICompleteRepository<LocalAuthority> localAuthoritiesRepo, IListAllProjectsQueryService listAllProjectsQueryService, ILogger<ListAllProjectByLocalAuthorities> logger)
+public class ListAllProjectByLocalAuthorities(ICompleteRepository<LocalAuthority> localAuthoritiesRepo, IListAllProjectsWithLAsQueryService ListAllProjectsWithLAsQueryService, ILogger<ListAllProjectByLocalAuthorities> logger)
     : IRequestHandler<ListAllProjectsByLocalAuthoritiesQuery, PaginatedResult<List<ListAllProjectLocalAuthoritiesResultModel>>>
 {
     public async Task<PaginatedResult<List<ListAllProjectLocalAuthoritiesResultModel>>> Handle(
-        ListAllProjectsByLocalAuthoritiesQuery request, CancellationToken cancellationToken)
+    ListAllProjectsByLocalAuthoritiesQuery request, CancellationToken cancellationToken)
     {
         try
         {
-            var localAuthorities = await localAuthoritiesRepo.FetchAsync(la => !string.IsNullOrEmpty(la.Code), cancellationToken);
+            var grouped = ListAllProjectsWithLAsQueryService.ListAllProjects(request.State, request.Type)
+                .GroupBy(p => p.LocalAuthority.Code)
+                .Select(g => new
+                {
+                    LocalAuthorityCode = g.Key,
+                    LocalAuthorityName = g.First().LocalAuthority.Name,
+                    ConversionCount = g.Count(p => p.Type == ProjectType.Conversion),
+                    TransferCount = g.Count(p => p.Type == ProjectType.Transfer)
+                });
 
-            var projectsWithEstablishments = await listAllProjectsQueryService.ListAllProjects(new ProjectFilters(request.State, request.Type)).ToListAsync(cancellationToken);
+            var count = await grouped.CountAsync(cancellationToken);
 
-            var localAuthoritiesWithProjectsDict = localAuthorities.OrderBy(la => la.Name).ToDictionary(
-                localAuthority => localAuthority,
-                localAuthority => projectsWithEstablishments
-                    .Where(p => p.Establishment.LocalAuthorityCode == localAuthority.Code).ToList());
+            var paginated = await grouped
+                .OrderBy(p => p.LocalAuthorityName)
+                .Paginate(request.Page, request.Count)
+                .ToListAsync(cancellationToken);
 
-            var filteredLocalAuthoritiesWithProjects = localAuthoritiesWithProjectsDict
-                .Where(entry => entry.Value.Count > 0);
+            var resultModel = paginated.Select(item => new ListAllProjectLocalAuthoritiesResultModel(
+                item.LocalAuthorityName,
+                item.LocalAuthorityCode,
+                item.ConversionCount,
+                item.TransferCount)).ToList();
 
-            var paginatedLocalAuthoritiesWithProjects = filteredLocalAuthoritiesWithProjects
-                .Skip(request.Page * request.Count)
-                .Take(request.Count);
-
-            var resultModel = paginatedLocalAuthoritiesWithProjects.Select(item =>
-                new ListAllProjectLocalAuthoritiesResultModel(
-                    item.Key,
-                    item.Key.Code,
-                    item.Value.Count(p => p.Project.Type == ProjectType.Conversion),
-                    item.Value.Count(p => p.Project.Type == ProjectType.Transfer)))
-                .ToList();
-
-            return PaginatedResult<List<ListAllProjectLocalAuthoritiesResultModel>>.Success(resultModel, filteredLocalAuthoritiesWithProjects.Count());
+            return PaginatedResult<List<ListAllProjectLocalAuthoritiesResultModel>>.Success(resultModel, count);
         }
         catch (Exception e)
         {
