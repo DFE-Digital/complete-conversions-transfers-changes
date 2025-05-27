291c10043f2c0ffe2f53049cee417b8b1719258e
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-05-27 12:12:23 +0100

    update directory

--- src/Frontend/Dfe.Complete/Pages/Projects/Notes/ViewProjectNotes.cshtml.cs
+++ src/Frontend/Dfe.Complete/Pages/Projects/Notes/ViewProjectNotes.cshtml.cs
@@ -1,7 +1,7 @@
 using Dfe.Complete.Pages.Projects.ProjectView;
 using MediatR;
 
-namespace Dfe.Complete.Pages.Projects
+namespace Dfe.Complete.Pages.Projects.Notes
 {
     public class ViewProjectNotesModel(ISender sender) : ProjectLayoutModel(sender, NotesNavigation);
 }
