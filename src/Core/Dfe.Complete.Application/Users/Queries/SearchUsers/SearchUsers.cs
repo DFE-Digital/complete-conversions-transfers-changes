9e4f7fcf81411a22ee8b38f4bd877f93ed3644b9
Nick Warms <Nicholas.WARMS@education.gov.uk>
2025-06-06 11:50:45 +0100

    Add search user query

---
+++ src/Core/Dfe.Complete.Application/Users/Queries/SearchUsers/SearchUsers.cs
@@ -0,0 +1,35 @@
+﻿using Dfe.Complete.Application.Common.Models;
+using Dfe.Complete.Domain.Entities;
+using Dfe.Complete.Domain.Interfaces.Repositories;
+using MediatR;
+
+namespace Dfe.Complete.Application.Users.Queries.SearchUsers
+{ 
+    public record SearchUsersQuery(
+       string Email) : PaginatedRequest<PaginatedResult<List<User>>>;
+
+    public class SearchUsersQueryHandler(
+        ICompleteRepository<User> users)
+        : IRequestHandler<SearchUsersQuery, PaginatedResult<List<User>>>
+    {
+        public async Task<PaginatedResult<List<User>>> Handle(SearchUsersQuery request,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                var searchQuery = await users.FetchAsync(user => user.Email != null && user.Email.Contains(request.Email), cancellationToken);
+
+                var itemCount = searchQuery.Count;
+
+                var userList = searchQuery
+                    .Skip(request.Page * request.Count).Take(request.Count).ToList(); 
+
+                return PaginatedResult<List<User>>.Success(userList, itemCount);
+            }
+            catch (Exception ex)
+            {
+                return PaginatedResult<List<User>>.Failure(ex.Message);
+            }
+        }
+    }
+}
