045878a9846dbb93478952f6a92501e236ddb41b
Nick Warms <Nicholas.WARMS@education.gov.uk>
2025-06-12 16:07:59 +0100

    Add controller test and GetUserById tests

--- src/Tests/Dfe.Complete.Application.Tests/QueryHandlers/User/GetUserByIdQueryHandlerTests.cs
+++ src/Tests/Dfe.Complete.Application.Tests/QueryHandlers/User/GetUserByIdQueryHandlerTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq.Expressions;
-using AutoFixture;
+﻿using AutoFixture;
 using AutoFixture.Xunit2;
 using AutoMapper;
 using Dfe.Complete.Application.Projects.Models;
@@ -7,6 +6,7 @@ using Dfe.Complete.Application.Users.Queries.GetUser;
 using Dfe.Complete.Domain.Entities;
 using Dfe.Complete.Domain.Enums;
 using Dfe.Complete.Domain.Interfaces.Repositories;
+using Dfe.Complete.Domain.ValueObjects;
 using Dfe.Complete.Tests.Common.Customizations.Behaviours;
 using Dfe.Complete.Tests.Common.Customizations.Models;
 using DfE.CoreLibs.Testing.AutoFixture.Attributes;
@@ -15,7 +15,7 @@ using NSubstitute;
 
 namespace Dfe.Complete.Application.Tests.QueryHandlers.User;
 
-public class GetUserByEmailQueryHandlerTests
+public class GetUserByIdQueryHandlerTests
 {
     [Theory]
     [CustomAutoData(typeof(DateOnlyCustomization), typeof(UserCustomization),
@@ -23,7 +23,7 @@ public class GetUserByEmailQueryHandlerTests
     public async Task Handle_ShouldReturnSuccess_WhenUserIsFound(
         [Frozen] ICompleteRepository<Domain.Entities.User> mockUserRepository,
         [Frozen] IMapper mockMapper,
-        GetUserByEmailQueryHandler handler,
+        GetUserByIdQueryHandler handler,
         Domain.Entities.User user,
         GiasEstablishment establishment,
         IFixture fixture)
@@ -48,7 +48,7 @@ public class GetUserByEmailQueryHandlerTests
         user.Email ??= "test@email.com";
 
         mockUserRepository
-            .FindAsync(Arg.Any<Expression<Func<Domain.Entities.User, bool>>>(), Arg.Any<CancellationToken>())
+            .GetAsync(Arg.Is<UserId>(i => i == user.Id), Arg.Any<CancellationToken>())
             .Returns(user);
 
         var userDto = new UserDto
@@ -59,7 +59,7 @@ public class GetUserByEmailQueryHandlerTests
 
         mockMapper.Map<UserDto>(user).Returns(userDto);
 
-        var query = new GetUserByEmailQuery(user.Email);
+        var query = new GetUserByIdQuery(user.Id);
 
         // Act
         var result = await handler.Handle(query, CancellationToken.None);
