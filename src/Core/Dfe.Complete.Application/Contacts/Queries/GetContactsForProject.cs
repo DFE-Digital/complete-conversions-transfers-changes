f3618a42ef90298099e3c074a3c1eeb0f9492992
Nick Warms <Nicholas.WARMS@education.gov.uk>
2025-06-17 13:46:12 +0100

    WIP add query for fetching contacts for a project

---
+++ src/Core/Dfe.Complete.Application/Contacts/Queries/GetContactsForProject.cs
@@ -0,0 +1,19 @@
+﻿using Dfe.Complete.Application.Common.Models;
+using Dfe.Complete.Domain.Entities;
+using Dfe.Complete.Domain.Interfaces.Repositories;
+using Dfe.Complete.Domain.ValueObjects;
+using MediatR;
+
+namespace Dfe.Complete.Application.Contacts.Queries;
+
+public record GetContactsForProjectQuery(ProjectId ProjectId) : IRequest<Result<List<Contact>>>;
+
+
+public class GetContactsForProject(IRepository<Contact> contactsRepository) : IRequestHandler<GetContactsForProjectQuery, Result<List<Contact>>>
+{
+
+    public async Task<Result<List<Contact>>> Handle(GetContactsForProjectQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
