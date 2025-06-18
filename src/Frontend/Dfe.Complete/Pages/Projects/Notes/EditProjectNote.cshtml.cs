8b17be2a2e13e87378f8a0b2d1d315800d276c9f
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-06-18 23:32:02 +0100

    populate field with existing note

--- src/Frontend/Dfe.Complete/Pages/Projects/Notes/EditProjectNote.cshtml.cs
+++ src/Frontend/Dfe.Complete/Pages/Projects/Notes/EditProjectNote.cshtml.cs
@@ -1,3 +1,5 @@
+using Dfe.Complete.Application.Notes.Commands;
+using Dfe.Complete.Application.Notes.Queries;
 using Dfe.Complete.Constants;
 using Dfe.Complete.Domain.ValueObjects;
 using MediatR;
@@ -15,10 +17,24 @@ public class EditProjectNoteModel(ISender sender) : PageModel
     public required Guid NoteId { get; set; }
 
     [BindProperty(Name = "note-text")]
-    public string NoteText { get; set; }
+    public required string NoteText { get; set; }
 
-    public async Task OnGet()
+    public async Task<IActionResult> OnGetAsync()
     {
+        if (NoteId == Guid.Empty)
+        {
+            return NotFound();
+        }
+
+        var noteResult = await sender.Send(new GetNoteByIdQuery(new NoteId(NoteId)));
+        if (!noteResult.IsSuccess || noteResult.Value == null)
+        {
+            return NotFound();
+        }
+
+        NoteText = noteResult.Value.Body;
+
+        return Page();
     }
 
     public async Task<IActionResult> OnPostAsync()
