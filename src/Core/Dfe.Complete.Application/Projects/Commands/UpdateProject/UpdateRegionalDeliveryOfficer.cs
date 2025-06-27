7020aced234da97031e8204464198c91d1d3620a
Nick Warms <Nicholas.WARMS@education.gov.uk>
2025-06-27 16:58:40 +0100

    Update formatting

--- src/Core/Dfe.Complete.Application/Projects/Commands/UpdateProject/UpdateRegionalDeliveryOfficer.cs
+++ src/Core/Dfe.Complete.Application/Projects/Commands/UpdateProject/UpdateRegionalDeliveryOfficer.cs
@@ -10,14 +10,15 @@ namespace Dfe.Complete.Application.Projects.Commands.UpdateProject;
 public record UpdateRegionalDeliveryOfficerCommand(
     ProjectId ProjectId,
     UserId RegionalDeliveryOfficer
-    ) : IRequest<Result<bool>>;
+) : IRequest<Result<bool>>;
 
 public class UpdateRegionalDeliveryOfficer(
     ICompleteRepository<Project> projectRepository,
     ICompleteRepository<User> userRepository)
     : IRequestHandler<UpdateRegionalDeliveryOfficerCommand, Result<bool>>
 {
-    public async Task<Result<bool>> Handle(UpdateRegionalDeliveryOfficerCommand request, CancellationToken cancellationToken)
+    public async Task<Result<bool>> Handle(UpdateRegionalDeliveryOfficerCommand request,
+        CancellationToken cancellationToken)
     {
         var project = await projectRepository.FindAsync(p => p.Id == request.ProjectId, cancellationToken);
         var user = await userRepository.GetAsync(request.RegionalDeliveryOfficer, cancellationToken);
@@ -25,9 +26,12 @@ public class UpdateRegionalDeliveryOfficer(
         {
             throw new NotFoundException("Email is not assignable", "email");
         }
+
         project.RegionalDeliveryOfficerId = request.RegionalDeliveryOfficer;
         project.RegionalDeliveryOfficer = user;
         var success = await projectRepository.UpdateAsync(project, cancellationToken);
-        return success.RegionalDeliveryOfficerId != request.RegionalDeliveryOfficer ? Result<bool>.Failure("User has not been updated") : Result<bool>.Success(true);
+        return success.RegionalDeliveryOfficerId != request.RegionalDeliveryOfficer
+            ? Result<bool>.Failure("User has not been updated")
+            : Result<bool>.Success(true);
     }
 }
