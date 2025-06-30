5cdffd112688f0c0cd1d9bf4af283b5fd8e6256a
Nick Warms <Nicholas.WARMS@education.gov.uk>
2025-06-30 15:54:36 +0100

    Momve policy to app settings

--- src/Frontend/Dfe.Complete/Security/CustomPolicies.cs
+++ src/Frontend/Dfe.Complete/Security/CustomPolicies.cs
@@ -39,16 +39,6 @@ public static class CustomPolicies
                     user.IsInRole(UserRolesConstants.ManageTeam) &&
                     (user.IsInRole(UserRolesConstants.RegionalCaseworkServices) || user.IsInRole(UserRolesConstants.RegionalDeliveryOfficer));
             });
-        },
-        [UserPolicyConstants.CanEditAddedByUser] = builder =>
-        {
-            builder.RequireAuthenticatedUser();
-            builder.RequireAssertion(context =>
-            {
-                var user = context.User;
-                return
-                    user.IsInRole(UserRolesConstants.ManageTeam) || user.IsInRole(UserRolesConstants.ServiceSupport);
-            });
-        },
+        }
     };
 }
