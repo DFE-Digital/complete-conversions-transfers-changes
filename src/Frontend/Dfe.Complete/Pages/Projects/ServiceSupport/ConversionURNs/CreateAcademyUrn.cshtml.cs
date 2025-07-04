61cdcfff1242c44cadb5acc8e068d2ef3845f10c
Elijah Aremu <45821029+elielijah321@users.noreply.github.com>
2025-07-04 16:03:44 +0100

    Feature/service support 187519 conversion urns (#312)
    * Testing corrections

--- src/Frontend/Dfe.Complete/Pages/Projects/ServiceSupport/ConversionURNs/CreateAcademyUrn.cshtml.cs
+++ src/Frontend/Dfe.Complete/Pages/Projects/ServiceSupport/ConversionURNs/CreateAcademyUrn.cshtml.cs
@@ -3,6 +3,7 @@ using Dfe.AcademiesApi.Client.Contracts;
 using Dfe.Complete.Application.Projects.Commands.UpdateProject;
 using Dfe.Complete.Application.Projects.Queries.GetEstablishment;
 using Dfe.Complete.Constants;
+using Dfe.Complete.Domain.Constants;
 using Dfe.Complete.Domain.ValueObjects;
 using Dfe.Complete.Extensions;
 using Dfe.Complete.Models;
@@ -10,9 +11,11 @@ using Dfe.Complete.Services;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Dfe.Complete.Validators;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Dfe.Complete.Pages.Projects.ServiceSupport.ConversionURNs
 {
+    [Authorize(policy: UserPolicyConstants.CanViewServiceSupport)]
     public class CreateAcademyUrnModel(ISender sender, 
         ErrorService errorService) : BaseProjectPageModel(sender)
     {
