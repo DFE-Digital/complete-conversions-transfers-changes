aa72a50066fe3f07c5f7f7ff6d0d3d6284591c0a
Elijah Aremu <45821029+elielijah321@users.noreply.github.com>
2025-07-09 10:48:13 +0100

    Feature/service support 187519 conversion urns (#316)
    * Use AcademyName from TaskData if present

--- src/Core/Dfe.Complete.Application/Projects/Queries/ListAllProjects/ListAllProjectsConverting.cs
+++ src/Core/Dfe.Complete.Application/Projects/Queries/ListAllProjects/ListAllProjectsConverting.cs
@@ -11,7 +11,7 @@ namespace Dfe.Complete.Application.Projects.Queries.ListAllProjects
 {
     public record ListAllProjectsConvertingQuery(bool WithAcademyUrn) : PaginatedRequest<PaginatedResult<IEnumerable<ListAllProjectsConvertingQueryResultModel>>>;
 
-    public class ListAllProjectsConvertingQueryHandler(IListAllProjectsQueryService listAllProjectsQueryService, ICompleteRepository<GiasEstablishment> establishmentRepo)
+    public class ListAllProjectsConvertingQueryHandler(IListAllProjectsQueryService listAllProjectsQueryService, ICompleteRepository<GiasEstablishment> establishmentRepo, ICompleteRepository<ConversionTasksData> taskDataRepo)
         : IRequestHandler<ListAllProjectsConvertingQuery, PaginatedResult<IEnumerable<ListAllProjectsConvertingQueryResultModel>>>
     {
         public async Task<PaginatedResult<IEnumerable<ListAllProjectsConvertingQueryResultModel>>> Handle(ListAllProjectsConvertingQuery request, CancellationToken cancellationToken)
@@ -20,31 +20,38 @@ namespace Dfe.Complete.Application.Projects.Queries.ListAllProjects
             {
                 var orderBy = new OrderProjectQueryBy() { Field = OrderProjectByField.SignificantDate, Direction = OrderByDirection.Ascending };
                 
-                var projectsQuery = listAllProjectsQueryService.ListAllProjects(new ProjectFilters(Domain.Enums.ProjectState.Active, Domain.Enums.ProjectType.Conversion, WithAcademyUrn: request.WithAcademyUrn), null, orderBy);
+                var projectsQuery = listAllProjectsQueryService
+                    .ListAllProjects(new ProjectFilters(ProjectState.Active, ProjectType.Conversion, WithAcademyUrn: request.WithAcademyUrn), null, orderBy);
                 var totalProjectCount = await projectsQuery.CountAsync(cancellationToken);
                 
                 var convertingProjects = await projectsQuery
                     .Skip(request.Page * request.Count)
                     .Take(request.Count)
                     .ToListAsync(cancellationToken);
+                
+                var taskDataIds = convertingProjects.Select(p => p.Project.TasksDataId).ToList();
+                var taskData = await taskDataRepo.FetchAsync(t => taskDataIds.Contains(t.Id), cancellationToken);
                
                List<GiasEstablishment> giasEstablishments = new List<GiasEstablishment>();
                
                if (request.WithAcademyUrn)
                {
                    var academuUrns = convertingProjects.Select(p => p.Project.AcademyUrn).Distinct();
-               
                    giasEstablishments = (await establishmentRepo.FetchAsync(e => academuUrns.Contains(e.Urn), cancellationToken)).ToList();
                }
      
                 var projects = convertingProjects
                     .Select(item =>
                         {
+                            var academyName =
+                                taskData.FirstOrDefault(t => t.Id == item.Project.TasksDataId)?.AcademyDetailsName ??
+                                giasEstablishments.FirstOrDefault(e => e.Urn == item.Project.AcademyUrn)?.Name;
+                            
                             return new ListAllProjectsConvertingQueryResultModel(item.Project.Id,
                                 item.Establishment?.Name, 
                                 item.Establishment?.Urn?.Value, 
                                 item.Project.SignificantDate,
-                                giasEstablishments.FirstOrDefault(e => e.Urn == item.Project.AcademyUrn)?.Name,
+                                academyName,
                                 item.Project.AcademyUrn?.Value);
                         }
                     );
