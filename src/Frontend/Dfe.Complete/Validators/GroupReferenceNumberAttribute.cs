8be0eb7ebd3c4cde8df21092b03134f71fc18ec4
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-08-29 10:56:01 +0100

    resolve code smells

--- src/Frontend/Dfe.Complete/Validators/GroupReferenceNumberAttribute.cs
+++ src/Frontend/Dfe.Complete/Validators/GroupReferenceNumberAttribute.cs
@@ -13,73 +13,56 @@ namespace Dfe.Complete.Validators
         protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
         {
             var groupReferenceNumber = value as string;
-
             if (string.IsNullOrEmpty(groupReferenceNumber))
                 return ValidationResult.Success!;
 
             string errorMessage = "A group reference number must start GRP_ and contain 8 numbers, like GRP_00000001";
-
             if (!groupReferenceNumber.StartsWith("GRP_"))
-            {
                 return new ValidationResult(errorMessage);
-            }
 
             var splitParts = groupReferenceNumber.Split("GRP_");
             if (splitParts.Length != 2)
                 return new ValidationResult(errorMessage);
 
             var numberPortionOfRefNumber = splitParts[1];
-
             if (!int.TryParse(numberPortionOfRefNumber, NumberStyles.None, CultureInfo.InvariantCulture, out _))
                 return new ValidationResult(errorMessage);
-
             if (numberPortionOfRefNumber.Length != 8)
                 return new ValidationResult(errorMessage);
 
             if (_shouldMatchWithTrustUkprn)
-            {
-                // Get the value of the ukprn
-                if (string.IsNullOrEmpty(_UkprnField))
-                {
-                    return new ValidationResult("UKPRN field name is not specified.");
-                }
+                return ValidateTrustUkprn(validationContext, groupReferenceNumber);
 
-                var ukprnProperty = validationContext.ObjectType.GetProperty(_UkprnField);
-
-                if (ukprnProperty == null)
-                {
-                    return new ValidationResult($"Property '{_UkprnField}' not found.");
-                }
-
-                var incomingUkprnValue = ukprnProperty.GetValue(validationContext.ObjectInstance)?.ToString();
-
-                if (string.IsNullOrEmpty(incomingUkprnValue))
-                {
-                    return new ValidationResult("Incoming trust ukprn cannot be empty");
-                }
-
-                var senderObj = validationContext.GetService(typeof(ISender));
-                if (senderObj is not ISender sender)
-                {
-                    return new ValidationResult("ISender service is not available in validation context.");
-                }
-
-                var result = sender.Send(new GetProjectGroupByGroupReferenceNumberQuery(groupReferenceNumber)).Result;
-
-                // No existing group
-                if (result == null || result.Value == null)
-                    return ValidationResult.Success!;
-
-                var trustUkprnObj = result.Value?.TrustUkprn;
-                var ukprn = trustUkprnObj?.Value.ToString();
-                if (ukprn != null && incomingUkprnValue.Equals(ukprn))
-                    return ValidationResult.Success!;
-
-                return new ValidationResult("The group reference number must be for the same trust as all other group members, check the group reference number and incoming trust UKPRN");
-            }
-
-            // If not matching with trust UKPRN, validation passes
             return ValidationResult.Success!;
+        }
+
+        private ValidationResult ValidateTrustUkprn(ValidationContext validationContext, string groupReferenceNumber)
+        {
+            if (string.IsNullOrEmpty(_UkprnField))
+                return new ValidationResult("UKPRN field name is not specified.");
+
+            var ukprnProperty = validationContext.ObjectType.GetProperty(_UkprnField);
+            if (ukprnProperty == null)
+                return new ValidationResult($"Property '{_UkprnField}' not found.");
+
+            var incomingUkprnValue = ukprnProperty.GetValue(validationContext.ObjectInstance)?.ToString();
+            if (string.IsNullOrEmpty(incomingUkprnValue))
+                return new ValidationResult("Incoming trust ukprn cannot be empty");
+
+            var senderObj = validationContext.GetService(typeof(ISender));
+            if (senderObj is not ISender sender)
+                return new ValidationResult("ISender service is not available in validation context.");
+
+            var result = sender.Send(new GetProjectGroupByGroupReferenceNumberQuery(groupReferenceNumber)).Result;
+            if (result == null || result.Value == null)
+                return ValidationResult.Success!;
+
+            var trustUkprnObj = result.Value?.TrustUkprn;
+            var ukprn = trustUkprnObj?.Value.ToString();
+            if (ukprn != null && incomingUkprnValue.Equals(ukprn))
+                return ValidationResult.Success!;
+
+            return new ValidationResult("The group reference number must be for the same trust as all other group members, check the group reference number and incoming trust UKPRN");
         }
     }
 }
