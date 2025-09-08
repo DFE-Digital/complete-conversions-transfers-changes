a5bbfea5c9a2c2894d7692aff2fb54f4b57b19ba
jignesh-jethwani-dfe <jignesh.jethwani@education.gov.uk>
2025-09-08 14:24:31 +0100

    formatted code

--- src/Frontend/Dfe.Complete/Validators/OtherExternalContactInputValidator.cs
+++ src/Frontend/Dfe.Complete/Validators/OtherExternalContactInputValidator.cs
@@ -1,6 +1,5 @@
 ﻿using Dfe.Complete.Domain.Enums;
 using Dfe.Complete.Models.ExternalContact;
-using Dfe.Complete.Services;
 using Dfe.Complete.Utils;
 using FluentValidation;
 
@@ -16,11 +15,14 @@ namespace Dfe.Complete.Validators
             RuleFor(x => x.Role)
                .NotEmpty().WithMessage("Enter a role");
 
-            When(x => x.SelectedExternalContactType == ExternalContactType.Solicitor.ToDescription() ||
-                    x.SelectedExternalContactType == ExternalContactType.Diocese.ToDescription() ||
-                    x.SelectedExternalContactType == ExternalContactType.Other.ToDescription(), () => {
-                RuleFor(x => x.IsPrimaryProjectContact).Equal(false).WithMessage("Only the incoming trust, outgoing trust, school or academy and local authority categories can have a primary contact.");
-            });
+            When(
+                    x => x.SelectedExternalContactType == ExternalContactType.Solicitor.ToDescription()
+                    || x.SelectedExternalContactType == ExternalContactType.Diocese.ToDescription()
+                    || x.SelectedExternalContactType == ExternalContactType.Other.ToDescription(), 
+                    () => {
+                    RuleFor(x => x.IsPrimaryProjectContact).Equal(false).WithMessage("Only the incoming trust, outgoing trust, school or academy and local authority categories can have a primary contact.");
+                    }
+                );
         }
     }
 }
