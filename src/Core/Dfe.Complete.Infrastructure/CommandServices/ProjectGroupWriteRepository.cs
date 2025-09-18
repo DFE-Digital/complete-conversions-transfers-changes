fbacbb99d3e5c31ffefcfdd458a5fdba6ec2acc6
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-09-18 16:46:51 +0100

    add common helper

---
+++ src/Core/Dfe.Complete.Infrastructure/CommandServices/ProjectGroupWriteRepository.cs
@@ -0,0 +1,17 @@
+using Dfe.Complete.Application.ProjectGroups.Interfaces;
+using Dfe.Complete.Domain.Entities;
+using Dfe.Complete.Infrastructure.Database;
+
+namespace Dfe.Complete.Infrastructure.CommandServices;
+
+internal class ProjectGroupWriteRepository(CompleteContext context) : IProjectGroupWriteRepository
+{
+    private readonly CompleteContext _context = context;
+
+    public async Task<ProjectGroup> CreateProjectGroupAsync(ProjectGroup projectGroup, CancellationToken cancellationToken)
+    {
+        await _context.ProjectGroups.AddAsync(projectGroup, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+        return projectGroup;
+    }
+}
