60fcd4e4800f295c06788df34bfd629e30f3c1bb
DEIFALLA <Mohamed.DEIFALLA@EDUCATION.GOV.UK>
2025-09-19 11:26:29 +0100

    hide add project button

--- src/Frontend/Dfe.Complete/Security/CustomPolicies.cs
+++ src/Frontend/Dfe.Complete/Security/CustomPolicies.cs
@@ -8,16 +8,10 @@ public static class CustomPolicies
 {
     public static Dictionary<string, Action<AuthorizationPolicyBuilder>> PolicyCustomizations => new()
     {
-        [UserPolicyConstants.CanCreateProjects] = builder =>
+        [UserPolicyConstants.CanCreateProjects] = builder => //Remove all access to create projects and any related sub-tasks, for example the URL projects/new
         {
             builder.RequireAuthenticatedUser();
-            builder.RequireAssertion(context =>
-            {
-                var user = context.User;
-                return
-                    user.IsInRole(UserRolesConstants.RegionalDeliveryOfficer) ||
-                    (user.IsInRole(UserRolesConstants.RegionalCaseworkServices) && !user.IsInRole(UserRolesConstants.ManageTeam));
-            });
+            builder.RequireAssertion(_ => false);
         },
         [UserPolicyConstants.CanViewTeamProjectsUnassigned] = builder =>
         {
