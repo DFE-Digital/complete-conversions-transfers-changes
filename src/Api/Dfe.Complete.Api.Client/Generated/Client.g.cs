099a52f213ac32d9770ab05cf643683c1ee2c59d
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-09-22 15:30:42 +0100

    add tests

--- src/Api/Dfe.Complete.Api.Client/Generated/Client.g.cs
+++ src/Api/Dfe.Complete.Api.Client/Generated/Client.g.cs
@@ -1419,9 +1419,9 @@ namespace Dfe.Complete.Client
         /// <param name="request">The request.</param>
         /// <returns>Project created successfully.</returns>
         /// <exception cref="CompleteApiException">A server side error occurred.</exception>
-        public virtual System.Threading.Tasks.Task<ProjectId> CreateConversionProject2Async(CreateConversionProjectCommand request)
+        public virtual System.Threading.Tasks.Task<ProjectId> CreateConversionProjectAsync(CreateConversionProjectCommand request)
         {
-            return CreateConversionProject2Async(request, System.Threading.CancellationToken.None);
+            return CreateConversionProjectAsync(request, System.Threading.CancellationToken.None);
         }
 
         /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
@@ -1431,7 +1431,7 @@ namespace Dfe.Complete.Client
         /// <param name="request">The request.</param>
         /// <returns>Project created successfully.</returns>
         /// <exception cref="CompleteApiException">A server side error occurred.</exception>
-        public virtual async System.Threading.Tasks.Task<ProjectId> CreateConversionProject2Async(CreateConversionProjectCommand request, System.Threading.CancellationToken cancellationToken)
+        public virtual async System.Threading.Tasks.Task<ProjectId> CreateConversionProjectAsync(CreateConversionProjectCommand request, System.Threading.CancellationToken cancellationToken)
         {
             if (request == null)
                 throw new System.ArgumentNullException("request");
