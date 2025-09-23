f95bd16c1ab889aab76375a32234481e876eb929
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-09-23 14:24:36 +0100

    add more tests

--- src/Tests/Dfe.Complete.Api.Tests.Integration/Controllers/ProjectsController/CreateProjectControllerTests.cs
+++ src/Tests/Dfe.Complete.Api.Tests.Integration/Controllers/ProjectsController/CreateProjectControllerTests.cs
@@ -234,7 +234,7 @@ public partial class ProjectsControllerTests
         typeof(DateOnlyCustomization),
         typeof(LocalAuthorityCustomization),
         typeof(CreateConversionProjectCommandCustomization))]
-    public async Task CreateHandoverConversionProject_Async_ShouldCreateHandoverConversionProject(
+    public async Task CreateHandoverConversionProject_Async_ShouldCreateHandoverConversionProjectOnly(
         CustomWebApplicationDbContextFactory<Program> factory,
         IProjectsClient projectsClient,
         IFixture fixture)
@@ -261,10 +261,114 @@ public partial class ProjectsControllerTests
         await dbContext.GiasEstablishments.AddAsync(giasEstablishment);
         await dbContext.SaveChangesAsync();
 
+        var projectCountBefore = await dbContext.Projects.CountAsync();
+        var userCountBefore = await dbContext.Users.CountAsync();
+        var conversionTaskDataCountBefore = await dbContext.ConversionTasksData.CountAsync();
+        var projectGroupCountBefore = await dbContext.ProjectGroups.CountAsync();
+
         var result = await projectsClient.CreateHandoverConversionProjectAsync(createHandoverConversionProjectCommand);
 
         Assert.NotNull(result);
         Assert.IsType<ProjectId>(result);
+        Assert.Equal(projectCountBefore + 1, await dbContext.Projects.CountAsync());
+        Assert.Equal(userCountBefore, await dbContext.Users.CountAsync());
+        Assert.Equal(conversionTaskDataCountBefore + 1, await dbContext.ConversionTasksData.CountAsync());
+        Assert.Equal(projectGroupCountBefore, await dbContext.ProjectGroups.CountAsync());
+
+        var createdProject = await dbContext.Projects.FirstOrDefaultAsync(p => p.Id == new Domain.ValueObjects.ProjectId(result.Value!.Value));
+        Assert.NotNull(createdProject);
+        Assert.Equal("Advisory board conditions", createdProject.AdvisoryBoardConditions);
+    }
+
+    [Theory]
+    [CustomAutoData(typeof(CustomWebApplicationDbContextFactoryCustomization),
+        typeof(DateOnlyCustomization),
+        typeof(LocalAuthorityCustomization),
+        typeof(CreateConversionProjectCommandCustomization))]
+    public async Task CreateHandoverConversionProject_Async_ShouldCreateProjectUserAndGroup(
+        CustomWebApplicationDbContextFactory<Program> factory,
+        IProjectsClient projectsClient,
+        IFixture fixture)
+    {
+        factory.TestClaims = [new Claim(ClaimTypes.Role, ApiRoles.WriteRole), new Claim(ClaimTypes.Role, ApiRoles.ReadRole)];
+        var dbContext = factory.GetDbContext<CompleteContext>();
+
+        var createHandoverConversionProjectCommand = GenerateCreateHandoverConversionCommand();
+        createHandoverConversionProjectCommand.GroupId = "GRP_99999999";
+
+        var localAuthority = dbContext.LocalAuthorities.AsEnumerable().MinBy(_ => Guid.NewGuid());
+        Assert.NotNull(localAuthority);
+
+        var giasEstablishment = fixture
+            .Customize(new GiasEstablishmentsCustomization()
+            {
+                LocalAuthority = localAuthority,
+                Urn = new Domain.ValueObjects.Urn(createHandoverConversionProjectCommand.Urn!.Value)
+            })
+            .Create<GiasEstablishment>();
+        await dbContext.GiasEstablishments.AddAsync(giasEstablishment);
+        await dbContext.SaveChangesAsync();
+
+        var projectCountBefore = await dbContext.Projects.CountAsync();
+        var userCountBefore = await dbContext.Users.CountAsync();
+        var conversionTaskDataCountBefore = await dbContext.ConversionTasksData.CountAsync();
+        var projectGroupCountBefore = await dbContext.ProjectGroups.CountAsync();
+
+        var result = await projectsClient.CreateHandoverConversionProjectAsync(createHandoverConversionProjectCommand);
+
+        Assert.NotNull(result);
+        Assert.IsType<ProjectId>(result);
+        Assert.Equal(projectCountBefore + 1, await dbContext.Projects.CountAsync());
+        Assert.Equal(userCountBefore + 1, await dbContext.Users.CountAsync());
+        Assert.Equal(conversionTaskDataCountBefore + 1, await dbContext.ConversionTasksData.CountAsync());
+        Assert.Equal(projectGroupCountBefore + 1, await dbContext.ProjectGroups.CountAsync());
+    }
+
+    [Theory]
+    [CustomAutoData(typeof(CustomWebApplicationDbContextFactoryCustomization),
+        typeof(DateOnlyCustomization),
+        typeof(LocalAuthorityCustomization),
+        typeof(CreateConversionProjectCommandCustomization))]
+    public async Task CreateHandoverConversionProject_Async_GroupUkprnDoesNotMatch_ShouldFailValidation(
+        CustomWebApplicationDbContextFactory<Program> factory,
+        IProjectsClient projectsClient,
+        IFixture fixture)
+    {
+        factory.TestClaims = [new Claim(ClaimTypes.Role, ApiRoles.WriteRole), new Claim(ClaimTypes.Role, ApiRoles.ReadRole)];
+        var dbContext = factory.GetDbContext<CompleteContext>();
+
+        var createHandoverConversionProjectCommand = GenerateCreateHandoverConversionCommand();
+        createHandoverConversionProjectCommand.GroupId = "GRP_88888888";
+
+        var localAuthority = dbContext.LocalAuthorities.AsEnumerable().MinBy(_ => Guid.NewGuid());
+        Assert.NotNull(localAuthority);
+
+        var giasEstablishment = fixture
+            .Customize(new GiasEstablishmentsCustomization()
+            {
+                LocalAuthority = localAuthority,
+                Urn = new Domain.ValueObjects.Urn(createHandoverConversionProjectCommand.Urn!.Value)
+            })
+            .Create<GiasEstablishment>();
+        await dbContext.GiasEstablishments.AddAsync(giasEstablishment);
+        await dbContext.ProjectGroups.AddAsync(new Domain.Entities.ProjectGroup
+        {
+            Id = new Domain.ValueObjects.ProjectGroupId(Guid.NewGuid()),
+            GroupIdentifier = "GRP_88888888",
+            TrustUkprn = new Domain.ValueObjects.Ukprn(11111111),
+        });
+
+        await dbContext.SaveChangesAsync();
+
+        var exception = await Assert.ThrowsAsync<CompleteApiException>(async () =>
+            await projectsClient.CreateHandoverConversionProjectAsync(createHandoverConversionProjectCommand));
+
+        Assert.Equal(HttpStatusCode.BadRequest, (HttpStatusCode)exception.StatusCode);
+
+        var validationErrors = exception.Response;
+        Assert.NotNull(validationErrors);
+
+        Assert.Contains($"Trust UKPRN 12129999 is not the same as the group UKPRN for group GRP_88888888", validationErrors);
     }
 
     [Theory]
