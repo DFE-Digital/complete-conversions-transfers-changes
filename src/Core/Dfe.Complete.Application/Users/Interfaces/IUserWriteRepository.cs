b6726116cc87d33a20c14b047610f3711e2e4dbd
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-09-26 16:22:54 +0100

    add edit user command handler

--- src/Core/Dfe.Complete.Application/Users/Interfaces/IUserWriteRepository.cs
+++ src/Core/Dfe.Complete.Application/Users/Interfaces/IUserWriteRepository.cs
@@ -5,4 +5,5 @@ namespace Dfe.Complete.Application.Users.Interfaces;
 public interface IUserWriteRepository
 {
     Task CreateUserAsync(User user, CancellationToken cancellationToken);
+    Task UpdateUserAsync(User user, CancellationToken cancellationToken);
 }
--- src/Core/Dfe.Complete.Domain/Constants/ErrorMessagesConstants.cs
+++ src/Core/Dfe.Complete.Domain/Constants/ErrorMessagesConstants.cs
@@ -7,8 +7,10 @@
         public const string CannotUpdateLocalAuthorityAsNotExisted = "Cannot update Local authority as it is not existed.";
         public const string ExceptionWhileUpdatingLocalAuthority = "Error occurred while updating local authority with ID {Id}.";
         public const string CannotDeleteLocalAuthorityAsLinkedToProject = "Cannot delete Local authority as it is linked to a project.";
+        public const string NotFoundUser = "User with Id {0} not found.";
         public const string NotFoundLocalAuthority = "Local authority with Id {0} not found.";
         public const string ExceptionWhileCreatingUser = "Error occurred while creating User with email {Email}.";
+        public const string ExceptionWhileUpdatingUser = "Error occurred while updating User with id {Id}.";
         public const string ExceptionWhileDeletingLocalAuthority = "Error occurred while deleting local authority with ID {Id}.";
         public const string ExceptionWhileCreatingLocalAuthority = "Error occurred while creating LocalAuthority with code {Code}.";
         public const string CouldNotCreateExternalContact = "Could not create external contact with project {ProjectId}.";
