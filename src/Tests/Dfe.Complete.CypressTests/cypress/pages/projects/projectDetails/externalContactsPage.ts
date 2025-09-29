4a6d1d45533e2bdcc7927b57c5a1a2dafebef1f6
paddy-clark <109068920+paddy-clark@users.noreply.github.com>
2025-09-29 12:59:13 +0100

    Automation/187371 external contact (#438)
    * remove project contacts on project deletion
    
    * fix duplicate id issue
    
    * change for consistency across app
    
    * add 'someone else' type external contact tests
    
    * fix duplicate id
    
    * add tests for preset external contact types
    
    * sonar fixes / test coverage

---
+++ src/Tests/Dfe.Complete.CypressTests/cypress/pages/projects/projectDetails/externalContactsPage.ts
@@ -0,0 +1,41 @@
+import { DetailsPage } from "cypress/pages/detailsPage";
+
+class ExternalContactsPage extends DetailsPage {
+    setContactItem(index: number) {
+        return this.setSectionCounter(index - 1);
+    }
+
+    setContactItemByRoleHeading(role: string) {
+        cy.getByClass(this.sectionItemClass).each(($el, index) => {
+            cy.wrap($el)
+                .find("h3")
+                .then(($h3) => {
+                    if ($h3.text().trim() === role) {
+                        cy.wrap(index - 1).as("sectionCounter");
+                        return false; // break
+                    }
+                });
+        });
+        return this;
+    }
+
+    containsOrganisationHeading(subHeading: string) {
+        cy.get("h3").contains(subHeading);
+        return this;
+    }
+
+    hasContactItem() {
+        return this.hasSectionItem();
+    }
+
+    hasRoleHeading(role: string) {
+        cy.get("@sectionCounter").then((index) => {
+            cy.getByClass(this.sectionItemClass).eq(Number(index)).find("h3").contains(role);
+        });
+        return this;
+    }
+}
+
+const externalContactsPage = new ExternalContactsPage();
+
+export default externalContactsPage;
