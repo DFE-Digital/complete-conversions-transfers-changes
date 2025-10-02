892d43f83edd8e9afd900dd191cd9e2f22e5f8e1
SHAKIR, Muhammad <Muhammad.SHAKIR@EDUCATION.GOV.UK>
2025-10-02 14:38:39 +0100

    fixed

--- src/Frontend/Dfe.Complete/Pages/Projects/TaskList/Tasks/MainContactTask/MainContactTask.cshtml.cs
+++ src/Frontend/Dfe.Complete/Pages/Projects/TaskList/Tasks/MainContactTask/MainContactTask.cshtml.cs
@@ -4,15 +4,13 @@ using Dfe.Complete.Domain.Enums;
 using Dfe.Complete.Domain.ValueObjects;
 using Dfe.Complete.Extensions;
 using Dfe.Complete.Models;
-using Dfe.Complete.Pages.Projects.TaskList.Tasks.DeclarationOfExpenditureCertificateTask;
-using Dfe.Complete.Services;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dfe.Complete.Pages.Projects.TaskList.Tasks.MainContactTask
 {
-    public class MainContactTaskModel(ISender sender, IAuthorizationService authorizationService, ILogger<DeclarationOfExpenditureCertificateTaskModel> logger, ErrorService errorService)
+    public class MainContactTaskModel(ISender sender, IAuthorizationService authorizationService, ILogger<MainContactTaskModel> logger)
     : BaseProjectTaskModel(sender, authorizationService, logger, NoteTaskIdentifier.MainContact)
     {
         [BindProperty]
@@ -21,7 +19,7 @@ namespace Dfe.Complete.Pages.Projects.TaskList.Tasks.MainContactTask
         public override async Task<IActionResult> OnGetAsync()
         {
             await base.OnGetAsync();
-             
+
             MainContactId = Project.MainContactId?.Value; 
             return Page();
         }
