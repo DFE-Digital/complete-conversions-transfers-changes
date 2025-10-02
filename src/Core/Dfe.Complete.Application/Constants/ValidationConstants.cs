0f97e2f065b30b6c058cef26fedc92eed5476786
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-10-02 08:48:17 +0100

    update validation constants

--- src/Core/Dfe.Complete.Application/Constants/ValidationConstants.cs
+++ src/Core/Dfe.Complete.Application/Constants/ValidationConstants.cs
@@ -2,6 +2,7 @@
 
 public static class ValidationConstants
 {
+    public const string MismatchedTrustInGroupValidationMessage = "Trust UKPRN {0} is not the same as the group UKPRN for group {1}";
     public const string NoTrustFoundValidationMessage = "There's no trust with that UKPRN. Check the number you entered is correct";
     public const string UrnExistsValidationMessage = "URN {0} already exists in active/inactive conversion projects";
 }
