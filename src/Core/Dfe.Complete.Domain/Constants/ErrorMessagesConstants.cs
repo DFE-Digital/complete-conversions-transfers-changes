383ac19e7327e2b19e2be088d84c300d3d5c70b1
tristancollen <159437649+tristancollen@users.noreply.github.com>
2025-10-03 10:49:33 +0100

    Feature/237864 create new user (#434)
    * rename user read repository
    
    * add user write repository
    
    * add create user handler
    
    * add end point for creating user
    
    * add ui for creating user
    
    * update security tests
    
    * add missing hint
    
    * add missing hint
    
    * add cypress data tags
    
    ---------
    
    Co-authored-by: Thomas McInnes <128510230+thomasmcinnes-nimble@users.noreply.github.com>

--- src/Core/Dfe.Complete.Domain/Constants/ErrorMessagesConstants.cs
+++ src/Core/Dfe.Complete.Domain/Constants/ErrorMessagesConstants.cs
@@ -2,11 +2,13 @@
 {
     public static class ErrorMessagesConstants
     {
+        public const string AlreadyExistsUserWithCode = "User with email '{0}' already exists.";
         public const string AlreadyExistedLocalAuthorityWithCode = "Already existed local authority with code {0}.";
         public const string CannotUpdateLocalAuthorityAsNotExisted = "Cannot update Local authority as it is not existed.";
         public const string ExceptionWhileUpdatingLocalAuthority = "Error occurred while updating local authority with ID {Id}.";
         public const string CannotDeleteLocalAuthorityAsLinkedToProject = "Cannot delete Local authority as it is linked to a project.";
         public const string NotFoundLocalAuthority = "Local authority with Id {0} not found.";
+        public const string ExceptionWhileCreatingUser = "Error occurred while creating User with email {Email}.";
         public const string ExceptionWhileDeletingLocalAuthority = "Error occurred while deleting local authority with ID {Id}.";
         public const string ExceptionWhileCreatingLocalAuthority = "Error occurred while creating LocalAuthority with code {Code}.";
         public const string CouldNotCreateExternalContact = "Could not create external contact with project {ProjectId}.";
