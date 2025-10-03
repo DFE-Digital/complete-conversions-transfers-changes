37bc17339049d36549c6dadcd4fd9fe00ae3b40e
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-10-03 15:36:20 +0100

    handle errors

--- src/Core/Dfe.Complete.Application/Users/Commands/UpdateUserCommand.cs
+++ src/Core/Dfe.Complete.Application/Users/Commands/UpdateUserCommand.cs
@@ -9,8 +9,6 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Dfe.Complete.Domain.Validators;
-using Dfe.Complete.Application.Users.Queries.GetUser;
-using Dfe.Complete.Domain.Entities;
 
 namespace Dfe.Complete.Application.Users.Commands
 {
@@ -22,44 +20,29 @@ namespace Dfe.Complete.Application.Users.Commands
        ProjectTeam Team) : IRequest<Result<bool>>;
 
     internal class UpdateUserCommandHandler(
-        ISender sender,
-        IUserWriteRepository userWriteRepository,
-        ILogger<UpdateUserCommandHandler> logger) : IRequestHandler<UpdateUserCommand, Result<bool>>
+       IUserReadRepository userReadRepository,
+       IUserWriteRepository userWriteRepository,
+       ILogger<UpdateUserCommandHandler> logger) : IRequestHandler<UpdateUserCommand, Result<bool>>
     {
         public async Task<Result<bool>> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
         {
             try
             {
-                var response = await sender.Send(new GetUserByIdQuery(request.Id), cancellationToken);
+                var existingUser = await new UserIdQuery(request.Id).Apply(userReadRepository.Users).FirstOrDefaultAsync(cancellationToken) ?? throw new NotFoundException(string.Format(ErrorMessagesConstants.NotFoundUser, request.Id));
 
-                if (!response.IsSuccess || response.Value == null)
-                {
-                    logger.LogWarning("{Message} UserId: {UserId}", ErrorMessagesConstants.NotFoundUser, request.Id);
-                    return Result<bool>.Failure(string.Format(ErrorMessagesConstants.NotFoundUser, request.Id), ErrorType.NotFound);
-                }
-
-                var existingUser = response.Value;
                 existingUser.FirstName = request.FirstName;
                 existingUser.LastName = request.LastName;
                 existingUser.Email = request.Email;
                 existingUser.Team = request.Team.ToDescription();
 
-                var userEntity = new User
-                {
-                    Id = existingUser.Id,
-                    FirstName = existingUser.FirstName,
-                    LastName = existingUser.LastName,
-                    Email = existingUser.Email,
-                    Team = existingUser.Team
-                };
-
-                await userWriteRepository.UpdateUserAsync(userEntity, cancellationToken);
+                await userWriteRepository.UpdateUserAsync(existingUser, cancellationToken);
                 return Result<bool>.Success(true);
             }
             catch (Exception ex)
             {
                 logger.LogError(ex, ErrorMessagesConstants.ExceptionWhileUpdatingUser, request.Id);
-                return Result<bool>.Failure(ex.Message);
+                var errorMessage = ex.InnerException?.Message ?? ex.Message;
+                return Result<bool>.Failure(errorMessage);
             }
         }
     }
