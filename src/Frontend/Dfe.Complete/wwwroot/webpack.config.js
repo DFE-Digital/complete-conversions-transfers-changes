85d6360c2c8138fa441bda922c88d5c289849f08
DEIFALLA <Mohamed.DEIFALLA@EDUCATION.GOV.UK>
2025-10-07 16:32:30 +0100

    fix footer

--- src/Frontend/Dfe.Complete/wwwroot/webpack.config.js
+++ src/Frontend/Dfe.Complete/wwwroot/webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
 	mode: 'production',
 	entry: ["./src/index.js", "./src/index.scss"],
 	plugins: [
 		new MiniCssExtractPlugin({ filename: 'site.css' }),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: 'src/assets/images/govuk-crest.svg',
+					to: 'assets/images/govuk-crest.svg'
+				}
+			]
+		})
 	],
 	module: {
 		rules: [
@@ -14,7 +24,12 @@ module.exports = {
 					// Creates `style` nodes from JS strings
 					MiniCssExtractPlugin.loader,
 					// Translates CSS into CommonJS
-					"css-loader",
+					{
+						loader: "css-loader",
+						options: {
+							url: true
+						}
+					},
 					// Compiles Sass to CSS
 					"sass-loader",
 				],
@@ -27,12 +42,14 @@ module.exports = {
 			},
 			{
 				test: /\.(gif|svg)$/i,
+				exclude: /govuk-crest\.svg$/,
 				use: [
 					{
 						loader: 'file-loader',
 						options: {
-							emitFile: false,
-							name: '/dist/assets/images/[name].[ext]'
+							emitFile: true,
+							name: '/dist/assets/images/[name].[ext]',
+							esModule: false
 						}
 					}
 				]
