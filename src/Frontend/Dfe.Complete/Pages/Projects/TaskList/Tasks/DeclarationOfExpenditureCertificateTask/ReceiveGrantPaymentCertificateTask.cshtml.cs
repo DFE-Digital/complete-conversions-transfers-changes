57cc387baeffb11ca30634b771f132856d316fa6
SHAKIR, Muhammad <Muhammad.SHAKIR@EDUCATION.GOV.UK>
2025-10-07 10:25:12 +0100

    refactored code

--- src/Frontend/Dfe.Complete/Pages/Projects/TaskList/Tasks/DeclarationOfExpenditureCertificateTask/ReceiveGrantPaymentCertificateTask.cshtml.cs
+++ src/Frontend/Dfe.Complete/Pages/Projects/TaskList/Tasks/DeclarationOfExpenditureCertificateTask/ReceiveGrantPaymentCertificateTask.cshtml.cs
@@ -1,13 +1,34 @@
+using Dfe.Complete.Application.Projects.Commands.TaskData;
+using Dfe.Complete.Constants;
 using Dfe.Complete.Domain.Enums;
+using Dfe.Complete.Domain.ValueObjects;
 using Dfe.Complete.Services.Interfaces;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
 namespace Dfe.Complete.Pages.Projects.TaskList.Tasks.DeclarationOfExpenditureCertificateTask
 {
     public class ReceiveGrantPaymentCertificateTaskModel(ISender sender, IAuthorizationService authorizationService, ILogger<ReceiveGrantPaymentCertificateTaskModel> logger, IErrorService errorService)
-    : BaseDeclarationOfExpenditureCertificateTaskModel(sender, authorizationService, logger, NoteTaskIdentifier.ReceiveGrantPaymentCertificate, errorService)
+    : BaseProjectTaskModel(sender, authorizationService, logger, NoteTaskIdentifier.ReceiveGrantPaymentCertificate)
     {
+        [BindProperty(Name = "not-applicable")]
+        public bool? NotApplicable { get; set; }
+        [BindProperty(Name = "saved")]
+        public bool? Saved { get; set; }
+
+        [BindProperty(Name = "check-certificate")]
+        public bool? CheckCertificate { get; set; }
+
+        [BindProperty(Name = "received-date")]
+        [DisplayName("Received date")]
+        public DateOnly? ReceivedDate { get; set; }
+
+        [BindProperty]
+        public Guid? TasksDataId { get; set; }
+        [BindProperty]
+        public ProjectType? Type { get; set; }
+
         public override async Task<IActionResult> OnGetAsync()
         {
             await base.OnGetAsync();
@@ -20,6 +41,23 @@ namespace Dfe.Complete.Pages.Projects.TaskList.Tasks.DeclarationOfExpenditureCer
             Saved = ConversionTaskData.ReceiveGrantPaymentCertificateSaveCertificate;
 
             return Page();
-        } 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!NotApplicable.HasValue && !(ReceivedDate?.ToDateTime(new TimeOnly()) < DateTime.Today))
+            {
+                ModelState.AddModelError("received-date", string.Format(ValidationConstants.DateInPast, "Received"));
+            }
+            if (!ModelState.IsValid)
+            {
+                await base.OnGetAsync();
+                errorService.AddErrors(ModelState);
+                return Page();
+            }
+            await Sender.Send(new UpdateDeclarationOfExpenditureCertificateTaskCommand(new TaskDataId(TasksDataId.GetValueOrDefault())!, Type, ReceivedDate, NotApplicable, CheckCertificate, Saved));
+            SetTaskSuccessNotification();
+            return Redirect(string.Format(RouteConstants.ProjectTaskList, ProjectId));
+        }
     }
 }
