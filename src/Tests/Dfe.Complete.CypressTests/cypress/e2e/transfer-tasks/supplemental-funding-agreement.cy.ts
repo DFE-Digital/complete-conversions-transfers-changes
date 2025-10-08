e4c6e6ce8f213b05a74590ce51d27179358509d5
paddy-clark <patrick.clark@education.gov.uk>
2025-10-08 15:38:29 +0100

    bug fixed

--- src/Tests/Dfe.Complete.CypressTests/cypress/e2e/transfer-tasks/supplemental-funding-agreement.cy.ts
+++ src/Tests/Dfe.Complete.CypressTests/cypress/e2e/transfer-tasks/supplemental-funding-agreement.cy.ts
@@ -48,8 +48,7 @@ describe("Transfer tasks - Supplemental funding agreement", () => {
             .hasDropdownContent("Changes that personalise the model documents to an academy or trust");
     });
 
-    // bug 236305
-    it.skip("should submit the form and persist selections", () => {
+    it("should submit the form and persist selections", () => {
         Logger.log("Select all checkboxes and save");
         taskPage
             .hasCheckboxLabel("Received")
