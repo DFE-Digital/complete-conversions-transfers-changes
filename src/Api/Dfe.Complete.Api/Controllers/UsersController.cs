b1633446dfc2ddeda5724c65c96ad075f8db541a
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-10-08 15:36:45 +0100

    add endpoint to update user

--- src/Api/Dfe.Complete.Api/Controllers/UsersController.cs
+++ src/Api/Dfe.Complete.Api/Controllers/UsersController.cs
@@ -29,9 +29,24 @@ namespace Dfe.Complete.Api.Controllers
         {
             var result = await sender.Send(request, cancellationToken);
 
-            if ( !result.IsSuccess || result.Value == null)
+            if (!result.IsSuccess || result.Value == null)
                 return BadRequest(result.Error);
             return Created("", result.Value!.Value);
+        }
+
+        /// <summary>
+        /// Updates a user.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        [Authorize(Policy = "CanReadWriteUpdate")]
+        [HttpPut]
+        [SwaggerResponse(204, "User updated successfully.")]
+        [SwaggerResponse(400, "Invalid request data.")]
+        public async Task<IActionResult> UpdateUserAsync([FromBody] UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            await sender.Send(request, cancellationToken);
+            return NoContent();
         }
 
         /// <summary>
