03bb277bd04fd9acd12dbaf2fe34086bd9f0ddb3
paddy-clark <patrick.clark@education.gov.uk>
2025-10-09 11:33:39 +0100

    214917 prepare handover update api requests

--- src/Tests/Dfe.Complete.CypressTests/cypress/api/prepareProjectApi.ts
+++ src/Tests/Dfe.Complete.CypressTests/cypress/api/prepareProjectApi.ts
@@ -8,34 +8,32 @@ import {
     CreateTransferPrepareResponse,
     PrepareProjectRequest,
 } from "cypress/api/apiDomain";
+import { EnvApi } from "cypress/constants/cypressConstants";
+import { ApiBase } from "cypress/api/apiBase";
 
-class PrepareProjectApi {
+class PrepareProjectApi extends ApiBase {
     public createConversionProject(
         request: CreateConversionPrepareRequest,
     ): Cypress.Chainable<CreateConversionPrepareResponse> {
-        return this.ProjectBaseRequest<CreateConversionPrepareResponse>("/projects/conversions", request, 201);
+        return this.ProjectBaseRequest<CreateConversionPrepareResponse>("/conversions", request, 201);
     }
 
     public createConversionFormAMatProject(
         request: CreateConversionFormAMatPrepareRequest,
     ): Cypress.Chainable<CreateConversionPrepareResponse> {
-        return this.ProjectBaseRequest<CreateConversionPrepareResponse>(
-            "/projects/conversions/form-a-mat",
-            request,
-            201,
-        );
+        return this.ProjectBaseRequest<CreateConversionPrepareResponse>("/conversions/form-a-mat", request, 201);
     }
 
     public createTransferProject(
         request: CreateTransferPrepareRequest,
     ): Cypress.Chainable<CreateTransferPrepareResponse> {
-        return this.ProjectBaseRequest<CreateTransferPrepareResponse>("/projects/transfers", request, 201);
+        return this.ProjectBaseRequest<CreateTransferPrepareResponse>("/transfers", request, 201);
     }
 
     public createTransferFormAMatProject(
         request: CreateTransferFormAMatPrepareRequest,
     ): Cypress.Chainable<CreateTransferPrepareResponse> {
-        return this.ProjectBaseRequest<CreateTransferPrepareResponse>("/projects/transfers/form-a-mat", request, 201);
+        return this.ProjectBaseRequest<CreateTransferPrepareResponse>("/transfers/form-a-mat", request, 201);
     }
 
     private ProjectBaseRequest<T extends CreatePrepareProjectResponse>(
@@ -43,22 +41,22 @@ class PrepareProjectApi {
         body: PrepareProjectRequest,
         expectedStatus: number = 201,
     ): Cypress.Chainable<T> {
-        return cy
-            .request<T>({
-                method: "POST",
-                headers: {
-                    apikey: Cypress.env("rubyApiKey"),
-                },
-                url: Cypress.env("rubyApi") + path,
-                body,
-            })
-            .then((response) => {
-                expect(
-                    response.status,
-                    `Expected POST request to return ${expectedStatus} but got ${response.status} with body ${response.body}`,
-                ).to.eq(expectedStatus);
-                return response.body;
-            });
+        return this.authenticatedRequest().then((headers) => {
+            return cy
+                .request<T>({
+                    method: "POST",
+                    url: Cypress.env(EnvApi) + "/v1/Projects/projects" + path,
+                    headers: headers,
+                    body,
+                })
+                .then((response) => {
+                    expect(
+                        response.status,
+                        `Expected POST request to return ${expectedStatus} but got ${response.status} with body ${response.body}`,
+                    ).to.eq(expectedStatus);
+                    return response.body;
+                });
+        });
     }
 }
 
