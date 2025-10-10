7e94dbfb32802e7ee7dca00215d768c6b545b36e
jignesh-jethwani-dfe <jignesh.jethwani@education.gov.uk>
2025-10-10 12:47:45 +0100

    updated delete external contact, update  keycontact reference to null if that contact is used in incoming trust ceo or outgoing trust ceo or headteacher or chair of governor

--- src/Core/Dfe.Complete.Application/Contacts/Commands/DeleteExternalContactCommand.cs
+++ src/Core/Dfe.Complete.Application/Contacts/Commands/DeleteExternalContactCommand.cs
@@ -2,11 +2,14 @@ using Dfe.Complete.Application.Common.Interfaces;
 using Dfe.Complete.Application.Common.Models;
 using Dfe.Complete.Application.Contacts.Interfaces;
 using Dfe.Complete.Application.Contacts.Queries.QueryFilters;
+using Dfe.Complete.Application.KeyContacts.Interfaces;
+using Dfe.Complete.Application.Projects.Interfaces;
 using Dfe.Complete.Domain.Constants;
 using Dfe.Complete.Domain.Entities;
 using Dfe.Complete.Domain.Interfaces.Repositories;
 using Dfe.Complete.Domain.ValueObjects;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace Dfe.Complete.Application.Contacts.Commands;
@@ -18,18 +21,20 @@ public class DeleteExternalContactCommandHandler(
     ICompleteRepository<Project> projectRepository,
     IContactReadRepository contactReadRepository,
     IContactWriteRepository contactWriteRepository,
+    IKeyContactReadRepository keyContactReadRepository,
+    IKeyContactWriteRepository keyContactWriteRepository,
     ILogger<DeleteExternalContactCommandHandler> logger
 ) : IRequestHandler<DeleteExternalContactCommand, Result<bool>>
 {
     public async Task<Result<bool>> Handle(DeleteExternalContactCommand request, CancellationToken cancellationToken)
     {
         try
-        {
+        {   
             await unitOfWork.BeginTransactionAsync();
 
-            var contactEntity = new ContactIdQuery(request.ContactId)
+            var contactEntity = await new ContactIdQuery(request.ContactId)
                 .Apply(contactReadRepository.Contacts)
-                .FirstOrDefault();
+                .FirstOrDefaultAsync();
 
             if (contactEntity is null)
             {
@@ -38,36 +43,11 @@ public class DeleteExternalContactCommandHandler(
                 return Result<bool>.Failure(message, ErrorType.NotFound);
             }
 
-            var project = await projectRepository.FindAsync(x => x.IncomingTrustMainContactId == request.ContactId
-           || x.EstablishmentMainContactId == request.ContactId || x.OutgoingTrustMainContactId == request.ContactId
-           || x.LocalAuthorityMainContactId == request.ContactId, cancellationToken);
-
-            if (project != null)
-            {
-                if (project.EstablishmentMainContactId == request.ContactId)
-                {
-                    project.EstablishmentMainContactId = null;
-                }
-
-                if (project.IncomingTrustMainContactId == request.ContactId)
-                {
-                    project.IncomingTrustMainContactId = null;
-                }
-
-                if (project.OutgoingTrustMainContactId == request.ContactId)
-                {
-                    project.OutgoingTrustMainContactId = null;
-                }
-
-                if (project.LocalAuthorityMainContactId == request.ContactId)
-                {
-                    project.LocalAuthorityMainContactId = null;
-                }
-
-                await projectRepository.UpdateAsync(project, cancellationToken);
-            }
+            await UnsetProjectContactReferencesAsync(request.ContactId, cancellationToken);
 
             await contactWriteRepository.RemoveContactAsync(contactEntity, cancellationToken);
+
+            await UnsetKeyContactReferencesAsync(request.ContactId, cancellationToken);
 
             await unitOfWork.CommitAsync();
 
@@ -75,12 +55,61 @@ public class DeleteExternalContactCommandHandler(
         }
         catch (Exception ex)
         {
-            await unitOfWork.RollBackAsync();           
+            await unitOfWork.RollBackAsync();
 
             var message = ErrorMessagesConstants.CouldNotDeleteExternalContact.Replace("{Id}", request.ContactId.Value.ToString());
             logger.LogError(ex, ErrorMessagesConstants.CouldNotDeleteExternalContact, request.ContactId);
 
             return Result<bool>.Failure(message);
         }
+    }
+
+    private async Task UnsetProjectContactReferencesAsync(ContactId contactId, CancellationToken cancellationToken)
+    {
+        var project = await projectRepository.FindAsync(x => x.IncomingTrustMainContactId == contactId
+            || x.EstablishmentMainContactId == contactId || x.OutgoingTrustMainContactId == contactId
+            || x.LocalAuthorityMainContactId == contactId, cancellationToken);
+
+        if (project == null) return;
+
+        if (project.EstablishmentMainContactId == contactId)
+            project.EstablishmentMainContactId = null;
+
+        if (project.IncomingTrustMainContactId == contactId)
+            project.IncomingTrustMainContactId = null;
+
+        if (project.OutgoingTrustMainContactId == contactId)
+            project.OutgoingTrustMainContactId = null;
+
+        if (project.LocalAuthorityMainContactId == contactId)
+            project.LocalAuthorityMainContactId = null;
+
+        await projectRepository.UpdateAsync(project, cancellationToken);
+    }
+
+    private async Task UnsetKeyContactReferencesAsync(ContactId contactId, CancellationToken cancellationToken)
+    {
+        var keycontact = await keyContactReadRepository.KeyContacts.Where(
+            n => n.IncomingTrustCeoId == contactId
+            || n.OutgoingTrustCeoId == contactId
+            || n.HeadteacherId == contactId
+            || n.ChairOfGovernorsId == contactId
+            ).FirstOrDefaultAsync(cancellationToken);
+
+        if (keycontact == null) return;
+
+        if (keycontact.IncomingTrustCeoId == contactId)
+            keycontact.IncomingTrustCeoId = null;
+
+        if (keycontact.OutgoingTrustCeoId == contactId)
+            keycontact.OutgoingTrustCeoId = null;
+
+        if (keycontact.HeadteacherId == contactId)
+            keycontact.HeadteacherId = null;
+
+        if (keycontact.ChairOfGovernorsId == contactId)
+            keycontact.ChairOfGovernorsId = null;
+
+        await keyContactWriteRepository.UpdateKeyContactAsync(keycontact, cancellationToken);
     }
 }
