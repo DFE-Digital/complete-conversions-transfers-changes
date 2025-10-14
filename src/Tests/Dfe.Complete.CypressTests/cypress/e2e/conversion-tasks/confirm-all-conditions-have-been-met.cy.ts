4e86534417f66146e87aa8339bad24b2db4b14ac
paddy-clark <patrick.clark@education.gov.uk>
2025-10-14 09:40:13 +0100

    confirm all conditions met task test

---
+++ src/Tests/Dfe.Complete.CypressTests/cypress/e2e/conversion-tasks/confirm-all-conditions-have-been-met.cy.ts
@@ -0,0 +1,76 @@
+import { ProjectBuilder } from "cypress/api/projectBuilder";
+import projectApi from "cypress/api/projectApi";
+import { checkAccessibilityAcrossPages } from "cypress/support/reusableTests";
+import taskListPage from "cypress/pages/projects/tasks/taskListPage";
+import projectRemover from "cypress/api/projectRemover";
+import { rdoLondonUser } from "cypress/constants/cypressConstants";
+import taskPage from "cypress/pages/projects/tasks/taskPage";
+import { Logger } from "cypress/common/logger";
+import { urnPool } from "cypress/constants/testUrns";
+
+const project = ProjectBuilder.createConversionProjectRequest({
+    urn: { value: urnPool.conversionTasks.spen },
+});
+let projectId: string;
+let taskId: string;
+const otherUserProject = ProjectBuilder.createConversionFormAMatProjectRequest({
+    urn: { value: urnPool.conversionTasks.grylls },
+    userAdId: rdoLondonUser.adId,
+});
+let otherUserProjectId: string;
+
+describe("Conversion tasks - Confirm all conditions have been met", () => {
+    before(() => {
+        projectRemover.removeProjectIfItExists(project.urn.value);
+        projectRemover.removeProjectIfItExists(otherUserProject.urn.value);
+        projectApi.createConversionProject(project).then((createResponse) => {
+            projectId = createResponse.value;
+            projectApi.getProject(project.urn.value).then((response) => {
+                taskId = response.body.tasksDataId.value;
+            });
+        });
+        projectApi.createMatConversionProject(otherUserProject).then((createResponse) => {
+            otherUserProjectId = createResponse.value;
+        });
+    });
+
+    beforeEach(() => {
+        cy.login();
+        cy.acceptCookies();
+        cy.visit(`projects/${projectId}/tasks/conditions_met`);
+    });
+
+    it("should expand and collapse guidance details", () => {
+        taskPage
+            .clickDropdown("How to check all conditions have been met")
+            .hasDropdownContent("legal documents are cleared")
+            .clickDropdown("What to do if conditions are not met")
+            .hasDropdownContent(
+                "You must agree a new conversion date with all stakeholders, then change the conversion date for this project.",
+            );
+    });
+
+    it("should submit the form and persist selections", () => {
+        Logger.log("Select the 'Confirm' checkbox and save");
+        taskPage.hasCheckboxLabel("Confirm all conditions are met").tick().saveAndReturn();
+        taskListPage
+            .hasTaskStatusCompleted("Confirm all conditions have been met")
+            .selectTask("Confirm all conditions have been met");
+
+        Logger.log("Unselect the 'Confirm' checkbox and save");
+        taskPage.hasCheckboxLabel("Confirm all conditions are met").isTicked().untick().saveAndReturn();
+        taskListPage
+            .hasTaskStatusNotStarted("Confirm all conditions have been met")
+            .selectTask("Confirm all conditions have been met");
+        taskPage.hasCheckboxLabel("Confirm all conditions are met").isUnticked();
+    });
+
+    it("Should NOT see the 'save and return' button for another user's project", () => {
+        cy.visit(`projects/${otherUserProjectId}/tasks/conditions_met`);
+        taskPage.noSaveAndReturnExists();
+    });
+
+    it("Check accessibility across pages", () => {
+        checkAccessibilityAcrossPages();
+    });
+});
