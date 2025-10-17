1d1bb23b5ca5cece707e1dd1560ab4e4977c287f
Elijah Aremu <mrelijaha@gmail.com>
2025-10-17 15:55:36 +0100

    Complete a notification of changes to funded high needs places form

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,7 @@ Security in case of vulnerabilities.
 ## Unreleased
 
 ### Added
+- Added `Complete a notification of changes to funded high needs places form` task for conversion projects.
 
 ### Changed
 
---
+++ src/Core/Dfe.Complete.Application/Projects/Commands/TaskData/UpdateCompleteNotificationOfChangeTaskCommand.cs
@@ -0,0 +1,39 @@
+﻿using Dfe.Complete.Application.Common.Models;
+using Dfe.Complete.Application.Notes.Interfaces;
+using Dfe.Complete.Application.Projects.Interfaces;
+using Dfe.Complete.Domain.ValueObjects;
+using Dfe.Complete.Utils;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dfe.Complete.Application.Projects.Commands.TaskData
+{
+    public record UpdateCompleteNotificationOfChangeTaskCommand(
+        TaskDataId TaskDataId,
+        bool? NotApplicable,
+        bool? TellLocalAuthority,
+        bool? CheckDocument,
+        bool? SendDocument
+    ) : IRequest<Result<bool>>;
+
+    internal class UpdateCompleteNotificationOfChangeTaskCommandHandler(
+        ITaskDataReadRepository taskDataReadRepository,
+        ITaskDataWriteRepository taskDataWriteRepository)
+        : IRequestHandler<UpdateCompleteNotificationOfChangeTaskCommand, Result<bool>>
+    {
+        public async Task<Result<bool>> Handle(UpdateCompleteNotificationOfChangeTaskCommand request, CancellationToken cancellationToken)
+        {
+            var tasksData = await taskDataReadRepository.ConversionTaskData.FirstOrDefaultAsync(p => p.Id == request.TaskDataId, cancellationToken)
+                            ?? throw new NotFoundException($"Conversion task data {request.TaskDataId} not found.");
+             
+            tasksData.CompleteNotificationOfChangeNotApplicable = request.NotApplicable;
+            tasksData.CompleteNotificationOfChangeTellLocalAuthority = request.NotApplicable == true ? null : request.TellLocalAuthority;
+            tasksData.CompleteNotificationOfChangeCheckDocument = request.NotApplicable == true ? null : request.CheckDocument;
+            tasksData.CompleteNotificationOfChangeSendDocument = request.NotApplicable == true ? null : request.SendDocument;
+
+            await taskDataWriteRepository.UpdateConversionAsync(tasksData, DateTime.UtcNow, cancellationToken);
+
+            return Result<bool>.Success(true);
+        }
+    }
+}
