a4b5d83e78ed339a5ac3c9201ed633ffbff0ca65
paddy-clark <patrick.clark@education.gov.uk>
2025-10-20 11:00:45 +0100

    add confirm the main contact task tests

--- src/Tests/Dfe.Complete.CypressTests/cypress/e2e/conversion-tasks/confirm-the-main-contact.cy.ts
+++ src/Tests/Dfe.Complete.CypressTests/cypress/e2e/conversion-tasks/confirm-the-main-contact.cy.ts
@@ -5,31 +5,24 @@ import projectRemover from "cypress/api/projectRemover";
 import projectApi from "cypress/api/projectApi";
 import taskPage from "cypress/pages/projects/tasks/taskPage";
 import { checkAccessibilityAcrossPages } from "cypress/support/reusableTests";
+import taskListPage from "cypress/pages/projects/tasks/taskListPage";
 
 const project = ProjectBuilder.createConversionProjectRequest({
     urn: { value: urnPool.conversionTasks.spen },
 });
 let projectId: string;
-const projectWithoutCEOContact = ProjectBuilder.createConversionProjectRequest({
-    urn: { value: urnPool.conversionTasks.huddersfield },
-});
-let projectWithoutCEOContactId: string;
 const otherUserProject = ProjectBuilder.createConversionFormAMatProjectRequest({
     urn: { value: urnPool.conversionTasks.grylls },
     userAdId: rdoLondonUser.adId,
 });
 let otherUserProjectId: string;
 
-describe("Conversion Tasks - Confirm the incoming trust CEO's details", () => {
+describe("Conversion Tasks - Confirm the main contact", () => {
     before(() => {
         projectRemover.removeProjectIfItExists(project.urn.value);
-        projectRemover.removeProjectIfItExists(projectWithoutCEOContact.urn.value);
         projectRemover.removeProjectIfItExists(otherUserProject.urn.value);
         projectApi.createConversionProject(project).then((createResponse) => {
             projectId = createResponse.value;
-        });
-        projectApi.createConversionProject(projectWithoutCEOContact).then((createResponse) => {
-            projectWithoutCEOContactId = createResponse.value;
         });
         projectApi.createMatConversionProject(otherUserProject).then((createResponse) => {
             otherUserProjectId = createResponse.value;
@@ -41,19 +34,14 @@ describe("Conversion Tasks - Confirm the incoming trust CEO's details", () => {
         cy.acceptCookies();
     });
 
-    // awaiting 238981 to add a contact via API
-    it.skip("Should", () => {
-        cy.visit(`projects/${projectId}/tasks/confirm_incoming_trust_ceo_contact`);
+    it("Should be able to choose contact and save the task", () => {
+        cy.visit(`projects/${projectId}/tasks/main_contact`);
+        taskPage.hasCheckboxLabel("Mr Tom Brailsford").tick().saveAndReturn();
+        taskListPage.hasTaskStatusCompleted("Confirm the main contact");
     });
 
-    it("Should see add contact button if no ceo exists", () => {
-        cy.visit(`projects/${projectWithoutCEOContactId}/tasks/confirm_incoming_trust_ceo_contact`);
-        taskPage.hasButton("Add a contact");
-    });
-
-    // awaiting 238981 to add a contact via API
-    it.skip("Should NOT see the 'save and return' button for another user's project", () => {
-        cy.visit(`projects/${otherUserProjectId}/tasks/confirm_incoming_trust_ceo_contact`);
+    it("Should NOT see the 'save and return' button for another user's project", () => {
+        cy.visit(`projects/${otherUserProjectId}/tasks/main_contact`);
         taskPage.noSaveAndReturnExists();
     });
 
