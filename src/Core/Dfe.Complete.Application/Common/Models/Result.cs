34f2563defec466fcc39399898c2c612c0ad0235
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-10-22 00:13:55 +0100

    exception handling

--- src/Core/Dfe.Complete.Application/Common/Models/Result.cs
+++ src/Core/Dfe.Complete.Application/Common/Models/Result.cs
@@ -1,30 +1,47 @@
-﻿namespace Dfe.Complete.Application.Common.Models
+﻿using Dfe.Complete.Utils;
+using Dfe.Complete.Utils.Exceptions;
+
+namespace Dfe.Complete.Application.Common.Models;
+
+public enum ErrorType
 {
-    public enum ErrorType
+    None,
+    Unauthorized,
+    NotFound,
+    Unknown,
+    UnprocessableContent
+
+}
+
+public class Result<T>
+{
+    public T? Value { get; }
+    public bool IsSuccess { get; }
+    public string? Error { get; }
+    public ErrorType ErrorType { get; }
+    public Exception? Exception { get; init; }
+
+    protected Result(T value, bool isSuccess, string? error, ErrorType errorType = ErrorType.None)
     {
-        None,
-        Unauthorized,
-        NotFound,
-        Unknown
+        Value = value;
+        IsSuccess = isSuccess;
+        Error = error;
+        ErrorType = errorType;
     }
 
-    public class Result<T>
+    public static Result<T> Success(T value) => new(value, true, null);
+    public static Result<T> Failure(string error, ErrorType errorType = ErrorType.Unknown)
     {
-        public T? Value { get; }
-        public bool IsSuccess { get; }
-        public string? Error { get; }
-        public ErrorType ErrorType { get; }
-
-        protected Result(T value, bool isSuccess, string? error, ErrorType errorType = ErrorType.None)
+        Exception? exception = errorType switch
         {
-            Value = value;
-            IsSuccess = isSuccess;
-            Error = error;
-            ErrorType = errorType;
-        }
+            ErrorType.NotFound => new NotFoundException(error),
+            ErrorType.UnprocessableContent => new UnprocessableContentException(error),
+            _ => new UnknownException(error),
+        };
 
-        public static Result<T> Success(T value) => new(value, true, null);
-        public static Result<T> Failure(string error, ErrorType errorType = ErrorType.Unknown) => new(default!, false, error, errorType);
+        return new(default!, false, error, errorType)
+        {
+            Exception = exception
+        };
     }
-
 }
