bfcbed1eaac14393984e5938503283ba0863203e
tristancollen <159437649+tristancollen@users.noreply.github.com>
2025-10-23 10:48:54 +0100

    add transfer creation end point (#466)
    * add transfer creation end point
    
    * update not found exception
    
    * deduplicate code
    
    * tidy code
    
    * update changelog
    
    * tmp changes
    
    * null the isDueToTwoRi field
    
    * move exceptions
    
    * update tests
    
    * exception handling
    
    * return unprocessable content
    
    * use unprocessable content exception
    
    * remove unused usings
    
    * unskip p2c transfer endpoint test
    
    ---------
    
    Co-authored-by: paddy-clark <patrick.clark@education.gov.uk>

--- src/Core/Dfe.Complete.Application/Projects/Commands/UpdateProject/UpdateAssignedUser.cs
+++ src/Core/Dfe.Complete.Application/Projects/Commands/UpdateProject/UpdateAssignedUser.cs
@@ -1,7 +1,7 @@
 ﻿using Dfe.Complete.Domain.Entities;
 using Dfe.Complete.Domain.Interfaces.Repositories;
 using Dfe.Complete.Domain.ValueObjects;
-using Dfe.Complete.Utils;
+using Dfe.Complete.Utils.Exceptions;
 using MediatR;
 
 namespace Dfe.Complete.Application.Projects.Commands.UpdateProject;
