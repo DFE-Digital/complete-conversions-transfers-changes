0a88929eae242aaf293cec13deacc1935575ddc8
SHAKIR, Muhammad <Muhammad.SHAKIR@EDUCATION.GOV.UK>
2025-10-27 11:16:57 +0000

    Added Deed Of Termination For the Church Supplemental Agreement task

--- src/Frontend/Dfe.Complete/Pages/Projects/TaskList/Tasks/DeedOfTerminationForChurchSupplementalAgreement/DeedOfTerminationForChurchSupplementalAgreement.cshtml.cs
+++ src/Frontend/Dfe.Complete/Pages/Projects/TaskList/Tasks/DeedOfTerminationForChurchSupplementalAgreement/DeedOfTerminationForChurchSupplementalAgreement.cshtml.cs
@@ -1,4 +1,7 @@
+using Dfe.Complete.Application.Projects.Commands.TaskData;
+using Dfe.Complete.Constants;
 using Dfe.Complete.Domain.Enums;
+using Dfe.Complete.Domain.ValueObjects;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -8,10 +11,52 @@ namespace Dfe.Complete.Pages.Projects.TaskList.Tasks.DeedOfTerminationForChurchS
     public class DeedOfTerminationForChurchSupplementalAgreementModel(ISender sender, IAuthorizationService authorizationService, ILogger<DeedOfTerminationForChurchSupplementalAgreementModel> logger)
     : BaseProjectTaskModel(sender, authorizationService, logger, NoteTaskIdentifier.DeedOfTerminationForChurchSupplementalAgreement)
     {
+        [BindProperty(Name = "not-applicable")]
+        public bool? NotApplicable { get; set; }
+
+        [BindProperty]
+        public bool? Received { get; set; }
+
+        [BindProperty]
+        public bool? Cleared { get; set; }
+
+        [BindProperty]
+        public bool? Signed { get; set; }
+
+        [BindProperty]
+        public bool? SignedByDiocese { get; set; }
+
+        [BindProperty(Name = "saved")]
+        public bool? Saved { get; set; }
+
+        [BindProperty]
+        public bool? SignedBySecretaryState { get; set; }
+
+        [BindProperty]
+        public bool? SavedAfterSigningBySecretaryState { get; set; }
+
+        [BindProperty]
+        public Guid? TasksDataId { get; set; }
         public override async Task<IActionResult> OnGetAsync()
         {
             await base.OnGetAsync();
+            NotApplicable = TransferTaskData.DeedTerminationChurchAgreementNotApplicable;
+            Received = TransferTaskData.DeedTerminationChurchAgreementReceived;
+            Cleared = TransferTaskData.DeedTerminationChurchAgreementCleared;
+            Signed = TransferTaskData.DeedTerminationChurchAgreementSignedOutgoingTrust;
+            SignedByDiocese = TransferTaskData.DeedTerminationChurchAgreementSignedDiocese;
+            Saved = TransferTaskData.DeedTerminationChurchAgreementSaved;
+            SignedBySecretaryState = TransferTaskData.DeedTerminationChurchAgreementSignedSecretaryState;
+            SavedAfterSigningBySecretaryState = TransferTaskData.DeedTerminationChurchAgreementSavedAfterSigningBySecretaryState;
+
             return Page();
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await Sender.Send(new UpdateDeedTerminationChurchSupplementalAgreementTaskCommand(new TaskDataId(TasksDataId.GetValueOrDefault())!,
+                NotApplicable, Received, Cleared, Signed, SignedByDiocese, Saved, SignedBySecretaryState, SavedAfterSigningBySecretaryState));
+            SetTaskSuccessNotification();
+            return Redirect(string.Format(RouteConstants.ProjectTaskList, ProjectId));
         }
     }
 }
