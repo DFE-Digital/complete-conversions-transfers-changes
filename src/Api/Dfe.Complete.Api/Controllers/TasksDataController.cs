30115dff5ae150dc59b37f58fd1ef3392c3c9800
SHAKIR, Muhammad <Muhammad.SHAKIR@EDUCATION.GOV.UK>
2025-10-28 16:04:15 +0000

    Added Subleases task

--- src/Api/Dfe.Complete.Api/Controllers/TasksDataController.cs
+++ src/Api/Dfe.Complete.Api/Controllers/TasksDataController.cs
@@ -608,5 +608,24 @@ namespace Dfe.Complete.Api.Controllers
             await sender.Send(request, cancellationToken);
             return NoContent();
         }
+        /// <summary>
+        /// Updating the subleases task data for conversion project.
+        /// </summary>
+        /// <param name="request">The update command.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        [Authorize(Policy = "CanReadWriteUpdate")]
+        [HttpPatch]
+        [Route("TaskData/Subleases")]
+        [SwaggerResponse(204, "Updated the subleases task successfully.")]
+        [SwaggerResponse(400, "Invalid request data.")]
+        [SwaggerResponse(404, "Project not found.")]
+        public async Task<IActionResult> UpdateSubleasesTaskAsync(
+            [FromBody] UpdateSubleasesTaskCommand request,
+            CancellationToken cancellationToken)
+        {
+            await sender.Send(request, cancellationToken);
+            return NoContent();
+        }
+        
     }
 }
--- src/Frontend/Dfe.Complete/Pages/Projects/TaskList/Tasks/SubleasesTask/SubleasesTask.cshtml.cs
+++ src/Frontend/Dfe.Complete/Pages/Projects/TaskList/Tasks/SubleasesTask/SubleasesTask.cshtml.cs
@@ -1,4 +1,7 @@
+using Dfe.Complete.Application.Projects.Commands.TaskData;
+using Dfe.Complete.Constants;
 using Dfe.Complete.Domain.Enums;
+using Dfe.Complete.Domain.ValueObjects;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -8,10 +11,43 @@ namespace Dfe.Complete.Pages.Projects.TaskList.Tasks.SubleasesTask
     public class SubleasesTaskModel(ISender sender, IAuthorizationService authorizationService, ILogger<SubleasesTaskModel> logger)
     : BaseProjectTaskModel(sender, authorizationService, logger, NoteTaskIdentifier.Subleases)
     {
+        [BindProperty(Name = "not-applicable")]
+        public bool? NotApplicable { get; set; }
+
+        [BindProperty]
+        public bool? Received { get; set; }
+
+        [BindProperty]
+        public bool? Cleared { get; set; }
+
+        [BindProperty]
+        public bool? Signed { get; set; } 
+
+        [BindProperty]
+        public bool? Saved { get; set; }
+
+        [BindProperty]
+        public bool? EmailSigned { get; set; }
+
+        [BindProperty]
+        public bool? SaveSigned { get; set; }
+        [BindProperty]
+        public bool? ReceiveSigned { get; set; } 
+
+        [BindProperty]
+        public Guid? TasksDataId { get; set; }
         public override async Task<IActionResult> OnGetAsync()
         {
             await base.OnGetAsync();
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await Sender.Send(new UpdateSubleasesTaskCommand(new TaskDataId(TasksDataId.GetValueOrDefault())!,
+                NotApplicable, Received, Cleared, Signed, Saved, EmailSigned, SaveSigned, ReceiveSigned));
+            SetTaskSuccessNotification();
+            return Redirect(string.Format(RouteConstants.ProjectTaskList, ProjectId));
         }
     }
 }
