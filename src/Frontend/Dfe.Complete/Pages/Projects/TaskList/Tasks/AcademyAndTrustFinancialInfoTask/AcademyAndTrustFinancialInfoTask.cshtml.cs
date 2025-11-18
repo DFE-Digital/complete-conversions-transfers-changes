b6bb721725122e297a784581773433372bb5783c
Elijah Aremu <mrelijaha@gmail.com>
2025-11-18 15:54:25 +0000

    Convert to Enum

--- src/Frontend/Dfe.Complete/Pages/Projects/TaskList/Tasks/AcademyAndTrustFinancialInfoTask/AcademyAndTrustFinancialInfoTask.cshtml.cs
+++ src/Frontend/Dfe.Complete/Pages/Projects/TaskList/Tasks/AcademyAndTrustFinancialInfoTask/AcademyAndTrustFinancialInfoTask.cshtml.cs
@@ -2,6 +2,7 @@ using Dfe.Complete.Application.Projects.Commands.TaskData;
 using Dfe.Complete.Constants;
 using Dfe.Complete.Domain.Enums;
 using Dfe.Complete.Domain.ValueObjects;
+using Dfe.Complete.Utils;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -20,9 +21,16 @@ namespace Dfe.Complete.Pages.Projects.TaskList.Tasks.AcademyAndTrustFinancialInf
         public string? TrustSurplusOrDeficit { get; set; }
         [BindProperty]
         public Guid? TasksDataId { get; set; }
+
+        public IEnumerable<AcademyAndTrustFinancialStatus> FinancialStatuses { get; set; } =
+            new List<AcademyAndTrustFinancialStatus>();
+        
         public override async Task<IActionResult> OnGetAsync()
         {
             await base.OnGetAsync();
+            
+            FinancialStatuses = EnumExtensions.ToList<AcademyAndTrustFinancialStatus>();
+            
             TasksDataId = Project.TasksDataId?.Value;
             NotApplicable = TransferTaskData.CheckAndConfirmFinancialInformationNotApplicable;
             AcademySurplusOrDeficit = TransferTaskData.CheckAndConfirmFinancialInformationAcademySurplusDeficit;
@@ -32,7 +40,11 @@ namespace Dfe.Complete.Pages.Projects.TaskList.Tasks.AcademyAndTrustFinancialInf
 
         public async Task<IActionResult> OnPost()
         {
-            await Sender.Send(new UpdateAcademyAndTrustFinancialInformationTaskCommand(new TaskDataId(TasksDataId.GetValueOrDefault())!, NotApplicable, AcademySurplusOrDeficit, TrustSurplusOrDeficit));
+            
+            var academySurplusOrDeficit = AcademySurplusOrDeficit == null ? null : EnumExtensions.FromDescriptionValue<AcademyAndTrustFinancialStatus>(AcademySurplusOrDeficit);
+            var trustSurplusOrDeficit = TrustSurplusOrDeficit == null ? null : EnumExtensions.FromDescriptionValue<AcademyAndTrustFinancialStatus>(TrustSurplusOrDeficit);
+
+            await Sender.Send(new UpdateAcademyAndTrustFinancialInformationTaskCommand(new TaskDataId(TasksDataId.GetValueOrDefault())!, NotApplicable, academySurplusOrDeficit, trustSurplusOrDeficit));
             SetTaskSuccessNotification();
             return Redirect(string.Format(RouteConstants.ProjectTaskList, ProjectId));
         }
