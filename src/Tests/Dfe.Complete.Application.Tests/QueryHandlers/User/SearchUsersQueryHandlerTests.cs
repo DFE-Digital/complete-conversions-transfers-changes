fa7ab4d57c5b49b7aa0f7b312e9636e32e3ef95c
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-11-21 10:26:45 +0000

    add filter fix

--- src/Tests/Dfe.Complete.Application.Tests/QueryHandlers/User/SearchUsersQueryHandlerTests.cs
+++ src/Tests/Dfe.Complete.Application.Tests/QueryHandlers/User/SearchUsersQueryHandlerTests.cs
@@ -1,16 +1,16 @@
 ﻿using AutoFixture;
 using AutoFixture.Xunit2;
+using Dfe.Complete.Application.Users.Interfaces;
 using Dfe.Complete.Application.Users.Queries.SearchUsers;
 using Dfe.Complete.Domain.Entities;
 using Dfe.Complete.Domain.Enums;
-using Dfe.Complete.Domain.Interfaces.Repositories;
 using Dfe.Complete.Domain.ValueObjects;
 using Dfe.Complete.Tests.Common.Customizations.Behaviours;
 using Dfe.Complete.Tests.Common.Customizations.Models;
 using GovUK.Dfe.CoreLibs.Testing.AutoFixture.Attributes;
 using GovUK.Dfe.CoreLibs.Testing.AutoFixture.Customizations;
+using MockQueryable.NSubstitute;
 using NSubstitute;
-using System.Linq.Expressions;
 
 namespace Dfe.Complete.Application.Tests.QueryHandlers.User;
 
@@ -19,8 +19,6 @@ public class SearchUsersQueryHandlerTests
     [Theory]
     [CustomAutoData(typeof(DateOnlyCustomization), typeof(UserCustomization), typeof(IgnoreVirtualMembersCustomisation))]
     public async Task Handle_ShouldReturnSuccess_WhenUserIsFound(
-        [Frozen] ICompleteRepository<Domain.Entities.User> mockUserRepository,
-        SearchUsersQueryHandler handler,
         Domain.Entities.User user,
         GiasEstablishment establishment,
         IFixture fixture)
@@ -42,15 +40,16 @@ public class SearchUsersQueryHandlerTests
             AssignedToId = user.Id
         }).Create<Domain.Entities.Project>();
 
-
         var projectList = new List<Domain.Entities.Project> { conversionProject, transferProject };
         user.ProjectAssignedTos = projectList;
         var userList = new List<Domain.Entities.User> { user };
 
-        mockUserRepository
-            .FetchAsync(Arg.Any<Expression<Func<Domain.Entities.User, bool>>>(), Arg.Any<CancellationToken>())
-            .Returns(userList);
+        // Mock IUserReadRepository
+        var mockUserReadRepository = Substitute.For<IUserReadRepository>();
+        var usersQueryable = userList.BuildMockDbSet();
+        mockUserReadRepository.Users.Returns(usersQueryable);
 
+        var handler = new SearchUsersQueryHandler(mockUserReadRepository);
         var query = new SearchUsersQuery(user.FullName);
 
         // Act
@@ -63,5 +62,35 @@ public class SearchUsersQueryHandlerTests
         Assert.Equal(user.Email, result.Value[0].Email);
         Assert.Equal(user.FullName, result.Value[0].FullName);
         Assert.Equivalent(user, result.Value[0]);
+    }
+
+    [Theory]
+    [CustomAutoData(typeof(DateOnlyCustomization), typeof(UserCustomization), typeof(IgnoreVirtualMembersCustomisation))]
+    public async Task Handle_ShouldFilterToAssignableUsers_WhenFilterToAssignableUsersIsTrue(
+        Domain.Entities.User assignableUser,
+        Domain.Entities.User nonAssignableUser)
+    {
+        // Arrange
+        assignableUser.Team = "london"; // RDO team
+        nonAssignableUser.Team = "data_consumers"; // Non-assignable team
+        
+        var userList = new List<Domain.Entities.User> { assignableUser, nonAssignableUser };
+
+        // Mock IUserReadRepository
+        var mockUserReadRepository = Substitute.For<IUserReadRepository>();
+        var usersQueryable = userList.BuildMockDbSet();
+        mockUserReadRepository.Users.Returns(usersQueryable);
+
+        var handler = new SearchUsersQueryHandler(mockUserReadRepository);
+        var query = new SearchUsersQuery("", FilterToAssignableUsers: true);
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.NotNull(result.Value);
+        Assert.Single(result.Value);
+        Assert.Equal(assignableUser.Email, result.Value[0].Email);
     }
 }
