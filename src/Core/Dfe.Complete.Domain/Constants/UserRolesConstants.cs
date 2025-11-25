ef55c98a2e22d9bdf07fd9bc23672ba13af1922f
Tristan Collen <tristan.collen@nimbleapproach.com>
2025-11-25 16:29:35 +0000

    update user policy checks

--- src/Core/Dfe.Complete.Domain/Constants/UserRolesConstants.cs
+++ src/Core/Dfe.Complete.Domain/Constants/UserRolesConstants.cs
@@ -2,10 +2,10 @@ namespace Dfe.Complete.Domain.Constants;
 
 public static class UserRolesConstants
 {
-    // Teams
+    public const string DataConsumers = "Data.Consumer";
     public const string RegionalCaseworkServicesTeamLead = "RegionalCaseworkServices.TeamLead";
     public const string RegionalCaseworkServices = "RegionalCaseworkServices";
+    public const string RegionalDeliveryOfficer = "RegionalDeliveryOfficer";
     public const string ServiceSupport = "Support.Service";
-    public const string DataConsumers = "Data.Consumer";
 }
 
