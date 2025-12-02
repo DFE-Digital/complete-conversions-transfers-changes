8f25a134da37f303291383d9218bdc11323a753e
paddy-clark <patrick.clark@education.gov.uk>
2025-12-02 15:45:26 +0000

    update search test

--- src/Tests/Dfe.Complete.CypressTests/cypress/pages/search.ts
+++ src/Tests/Dfe.Complete.CypressTests/cypress/pages/search.ts
@@ -1,10 +1,17 @@
 class Search {
-    private readonly searchId = "searchterm";
+    private readonly searchToggleId = "search-toggle-button";
+    private readonly searchInputId = "search-overlay-input";
     private readonly searchButtonId = "search-submit";
 
+    clickSearch() {
+        cy.getById(this.searchToggleId).click();
+        return this;
+    }
+
     searchFor(phrase: string) {
-        cy.getById(this.searchId).type(phrase);
+        cy.getById(this.searchInputId).type(phrase);
         cy.getById(this.searchButtonId).click();
+        return this;
     }
 }
 
