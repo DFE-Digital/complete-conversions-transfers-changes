9c70fb7febbbe4643dae93c614138af80dde9ddc
Tristan Collen <tristan.collen@nimbleapproach.com>
2026-02-12 10:07:51 +0000

    code smells

--- src/Frontend/Dfe.Complete/Models/TaskListItem.cs
+++ src/Frontend/Dfe.Complete/Models/TaskListItem.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Dfe.Complete.Models;
 
+[ExcludeFromCodeCoverage]
 public sealed record TaskListItemViewModel(
     string Name,
     string Link,
