30ba79b67090922ac40ef60bca829bedcb344f51
Tristan Collen <tristan.collen@nimbleapproach.com>
2026-03-29 08:48:58 +0100

    add command handler

---
+++ src/Core/Dfe.Complete.Application/Projects/Commands/UpdateProject/UpdateProjectOnHoldCommand.cs
@@ -0,0 +1,32 @@
+﻿using Dfe.Complete.Application.Common.Models;
+using Dfe.Complete.Domain.Entities;
+using Dfe.Complete.Domain.Enums;
+using Dfe.Complete.Domain.Interfaces.Repositories;
+using Dfe.Complete.Domain.ValueObjects;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dfe.Complete.Application.Projects.Commands.UpdateProject;
+
+public record UpdateProjectOnHoldCommand(
+    ProjectId ProjectId
+) : IRequest<Result<bool>>;
+
+internal class UpdateProjectOnHoldCommandHandler(
+    ICompleteRepository<Project> projectRepository)
+    : IRequestHandler<UpdateProjectOnHoldCommand, Result<bool>>
+{
+    public async Task<Result<bool>> Handle(UpdateProjectOnHoldCommand request,
+        CancellationToken cancellationToken)
+    {
+        var project = await projectRepository.Query()
+                .FirstOrDefaultAsync(x => x.Id == request.ProjectId, cancellationToken);
+
+        if (project == null)
+            return Result<bool>.Success(false);
+
+        project!.OnHoldDate = DateOnly.FromDateTime(DateTime.Now);
+        await projectRepository.UpdateAsync(project, cancellationToken);
+        return Result<bool>.Success(true);
+    }
+}
