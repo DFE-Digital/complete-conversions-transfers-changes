2ec9b19c35f2c1c0a9acaa1aaf98db64734ab7b6
Tristan Collen <tristan.collen@nimbleapproach.com>
2026-04-01 16:35:32 +0100

    update wording

--- src/Tests/Dfe.Complete.CypressTests/cypress/pages/projects/onHold.ts
+++ src/Tests/Dfe.Complete.CypressTests/cypress/pages/projects/onHold.ts
@@ -7,13 +7,14 @@ class OnHold extends TaskPage {
     }
 
     confirmHoldText(schoolName: string) {
-        this.contains(`You are about to put ${schoolName} on hold. This will prevent any changes being made to the project until you resume it.`);
+        this.contains(`You are about to put ${schoolName} on hold.`);
         return this;
     }
 
-    confirmResumeText(schoolName: string, projectType: 'conversion' | 'transfer') {
-        this.contains(`You are about to resume ${schoolName}. This will allow you to make changes to the project.`);
-        this.contains(`Check that the ${projectType} date is correct.`);
+    confirmResumeText(schoolName: string, projectType: 'conversion' | 'transfer', significantDate: string) {
+        this.contains(`You are about to resume ${schoolName}.`);
+        this.contains(`The current ${projectType} date is: ${significantDate}`);
+        this.contains(`Check that this date is correct when resuming the project.`);
         return this;
     }
 }
