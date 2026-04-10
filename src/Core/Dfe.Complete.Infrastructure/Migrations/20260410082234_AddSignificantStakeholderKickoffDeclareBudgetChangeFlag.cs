0cd674c075363dab493e28ad429ac751bf01719e
Tristan Collen <tristan.collen@nimbleapproach.com>
2026-04-10 15:45:13 +0100

    add new flag

---
+++ src/Core/Dfe.Complete.Infrastructure/Migrations/20260410082234_AddSignificantStakeholderKickoffDeclareBudgetChangeFlag.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.Complete.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSignificantStakeholderKickoffDeclareBudgetChangeFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "stakeholder_kick_off_declare_budget_changes",
+                schema: "complete",
+                table: "conversion_tasks_data",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "stakeholder_kick_off_declare_budget_changes",
+                schema: "complete",
+                table: "conversion_tasks_data");
+        }
+    }
+}
