b4503069ba20d6f8484ad75587e835f5d2cbaa2a
Tristan Collen <tristan.collen@nimbleapproach.com>
2026-04-16 14:38:12 +0100

    add migration

---
+++ src/Core/Dfe.Complete.Infrastructure/Migrations/20260416133442_AllConfirmStatutoryConsultationFields.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.Complete.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllConfirmStatutoryConsultationFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "statutory_consultation_complete  ",
+                schema: "complete",
+                table: "conversion_tasks_data",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "statutory_consultation_not_applicable",
+                schema: "complete",
+                table: "conversion_tasks_data",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "statutory_consultation_complete  ",
+                schema: "complete",
+                table: "conversion_tasks_data");
+
+            migrationBuilder.DropColumn(
+                name: "statutory_consultation_not_applicable",
+                schema: "complete",
+                table: "conversion_tasks_data");
+        }
+    }
+}
