e14564c07ae8601ba5876198bc548578f99e499d
Stephen Bradbury <82675733+SteBradbury@users.noreply.github.com>
2026-04-23 10:50:10 +0100

    Feature/275380 dbs checks (#715)
    * quick commit
    
    * added tests
    
    * updated changelog
    
    * merge fix
    
    * build fixes
    
    * code smell changes
    
    * regenerate of contracts
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Thomas McInnes <128510230+thomasmcinnes-nimble@users.noreply.github.com>

---
+++ src/Core/Dfe.Complete.Application/Projects/Commands/TaskData/UpdateConfirmDbsChecksTaskCommand.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Dfe.Complete.Application.Common.Models;
+using Dfe.Complete.Application.Notes.Interfaces;
+using Dfe.Complete.Application.Projects.Interfaces;
+using Dfe.Complete.Domain.ValueObjects;
+using Dfe.Complete.Utils.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Dfe.Complete.Application.Projects.Commands.TaskData
+{
+    public record UpdateConfirmDbsChecksTaskCommand(
+        [Required] TaskDataId TaskDataId,
+        bool? ConfirmDBSChecks
+    ) : IRequest<Result<bool>>;
+
+    internal class UpdateConfirmDbsChecksTaskCommandHandler(
+        ITaskDataReadRepository taskDataReadRepository,
+        ITaskDataWriteRepository taskDataWriteRepository)
+        : IRequestHandler<UpdateConfirmDbsChecksTaskCommand, Result<bool>>
+    {
+        public async Task<Result<bool>> Handle(UpdateConfirmDbsChecksTaskCommand request, CancellationToken cancellationToken)
+        {
+            var tasksData = await taskDataReadRepository.ConversionTaskData.FirstOrDefaultAsync(p => p.Id == request.TaskDataId,
+                                cancellationToken)
+                            ?? throw new NotFoundException($"Conversion task data {request.TaskDataId} not found.");
+
+            tasksData.ConfirmDBSChecks = request.ConfirmDBSChecks;
+
+            await taskDataWriteRepository.UpdateConversionAsync(tasksData, DateTime.UtcNow, cancellationToken);
+
+            return Result<bool>.Success(true);
+        }
+    }
+}
---
+++ src/Core/Dfe.Complete.Infrastructure/Migrations/20260420121133_AddConfirmDBSChecks.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.Complete.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConfirmDBSChecks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "confirm_dbs_checks",
+                schema: "complete",
+                table: "conversion_tasks_data",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "confirm_dbs_checks",
+                schema: "complete",
+                table: "conversion_tasks_data");
+        }
+    }
+}
