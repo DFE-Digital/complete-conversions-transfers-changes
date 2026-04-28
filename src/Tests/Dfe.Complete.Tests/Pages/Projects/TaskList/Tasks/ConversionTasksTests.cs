157a6837255994a80616c97414e9c4a35e553c9b
Stephen Bradbury <bradbury.ste@googlemail.com>
2026-04-28 09:13:01 +0100

    additional test

--- src/Tests/Dfe.Complete.Tests/Pages/Projects/TaskList/Tasks/ConversionTasksTests.cs
+++ src/Tests/Dfe.Complete.Tests/Pages/Projects/TaskList/Tasks/ConversionTasksTests.cs
@@ -35,5 +35,20 @@ namespace Dfe.Complete.Tests.Pages.Projects.TaskList.Tasks
             result.Should().OnlyHaveUniqueItems(x => x.DisplayOrder);
             result.First(x => x.Name == "Process conversion support grant").DisplayOrder.Should().Be(6);
         }
+
+        [Fact]
+        public void GetTasks_WithShowProcessConversionSupportGrantOfFalse_ShouldNotIncludeTask()
+        {
+            var conversionTaskList = new ConversionTaskListViewModel
+            {
+                ShowProcessConversionSupportGrant = false
+            };
+            var projectId = "project-123";
+            var result = ConversionTasks.BuildTaskList(conversionTaskList, projectId).ProjectKickoffTasks;
+
+            result.Should().HaveCountGreaterThan(1).And.BeInAscendingOrder(x => x.DisplayOrder);
+            result.Should().NotContain(x => x.Name == "Process conversion support grant");
+            result.Should().OnlyHaveUniqueItems(x => x.DisplayOrder);
+        }
     }
 }
