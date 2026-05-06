5c45d7355d35fb7f93e66159fc8550bb8e41bf43
Stephen Bradbury <82675733+SteBradbury@users.noreply.github.com>
2026-05-06 16:16:06 +0100

    Feature/277866 task ordering (#723)
    * quick commit
    
    * refactor and additional tests
    
    * removed unused code
    
    * additional test
    
    * moved things into correct folder
    
    * added back in La confirms payroll task
    
    * fixed ordering of tasks
    
    ---------
    
    Co-authored-by: Thomas McInnes <128510230+thomasmcinnes-nimble@users.noreply.github.com>

---
+++ src/Frontend/Dfe.Complete/Pages/Projects/TaskList/TaskLinkBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace Dfe.Complete.Pages.Projects.TaskList
+{
+    public class TaskLinkBuilder(string routeConstraints, string projectId)
+    {
+        public string Build(string task)
+        {
+            return string.Format(routeConstraints, projectId, task);
+        }
+    }
+}
