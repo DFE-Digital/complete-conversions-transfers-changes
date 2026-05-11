868fe356c16faac3e7cda991ce3d040f13abf38c
Tristan Collen <tristan.collen@nimbleapproach.com>
2026-05-11 10:23:34 +0100

    use existing api helper

--- src/Tests/Dfe.Complete.CypressTests/cypress/e2e/conversion-tasks/group-one/la-confirms-payroll-deadline.cy.ts
+++ src/Tests/Dfe.Complete.CypressTests/cypress/e2e/conversion-tasks/group-one/la-confirms-payroll-deadline.cy.ts
@@ -4,10 +4,17 @@ import taskPage from "cypress/pages/projects/tasks/taskPage";
 import { Logger } from "cypress/common/logger";
 import { ConversionTasksGroupOneSetup } from "cypress/support/conversionTasksSetup";
 import validationComponent from "cypress/pages/validationComponent";
-import stakeholderKickOffTaskPage from "cypress/pages/projects/tasks/stakeholderKickOffTaskPage";
+import taskHelperConversions from "cypress/api/taskHelperConversions";
+import { getSignificantDateString } from "cypress/support/formatDate";
 
 const taskPath = "la_confirms_payroll_deadline";
-const stakeholderKickOffTaskPath = "stakeholder_kick_off";
+
+const today = new Date();
+const dayTomorrow = String(today.getDate() + 1);
+const month = String(today.getMonth() + 1);
+const year = String(today.getFullYear());
+const yearTwoYearsFromNow = String(today.getFullYear() + 2);
+const significantDateNextYear = getSignificantDateString(12);
 
 describe("Conversion tasks - LA confirms payroll deadline", () => {
     let setup: ReturnType<typeof ConversionTasksGroupOneSetup.getSetup>;
@@ -20,10 +27,7 @@ describe("Conversion tasks - LA confirms payroll deadline", () => {
                 expect(setup.projectId, 'projectId should be set').to.not.be.empty;
             });
         }).then(() => {
-            cy.login();
-            cy.visit(`projects/${setup.projectId}/tasks/${stakeholderKickOffTaskPath}`);
-            stakeholderKickOffTaskPage.enterSignificantDate(1, new Date().getFullYear() + 1).saveAndReturn();
-            cy.visit(`projects/${setup.projectId}/tasks/${taskPath}`);
+            taskHelperConversions.updateExternalStakeholderKickOff(setup.projectId, "completed", significantDateNextYear);
         });
     });
 
@@ -32,11 +36,6 @@ describe("Conversion tasks - LA confirms payroll deadline", () => {
     });
 
     it("should be able to input a valid future date before the significant date", () => {
-        const today = new Date();
-        const dayTomorrow = String(today.getDate() + 1);
-        const month = String(today.getMonth() + 1);
-        const year = String(today.getFullYear());
-
         Logger.log("Input the date and save");
         taskPage.enterDate(dayTomorrow, month, year, "payroll-deadline").saveAndReturn();
         taskListPage
@@ -51,7 +50,7 @@ describe("Conversion tasks - LA confirms payroll deadline", () => {
         taskPage.hasDate("", "", "", "payroll-deadline");
     });
 
-    it.only("should show validation errors", () => {
+    it("should show validation errors", () => {
         Logger.log("Try to input a past date");
         taskPage
             .enterDate("15", "6", "2020", "payroll-deadline")
@@ -61,7 +60,7 @@ describe("Conversion tasks - LA confirms payroll deadline", () => {
 
         Logger.log("Try to input a date after the significant date");
         taskPage
-            .enterDate("15", "6", String(new Date().getFullYear() + 10), "payroll-deadline")
+            .enterDate("15", "6", yearTwoYearsFromNow, "payroll-deadline")
             .saveAndReturn()
 
         validationComponent.hasLinkedValidationError("Payroll deadline must be before the significant date");
