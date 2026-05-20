d15e7788e6ff66d9414f6b0e4e7993cc7903b192
Stephen Bradbury <bradbury.ste@googlemail.com>
2026-05-20 15:31:22 +0100

    fixes for image rebrand

--- src/Frontend/Dfe.Complete/wwwroot/webpack.assets.config.js
+++ src/Frontend/Dfe.Complete/wwwroot/webpack.assets.config.js
@@ -13,15 +13,8 @@ module.exports = {
 				{ from: path.join(__dirname, 'node_modules/govuk-frontend/dist/govuk/assets'), to: path.join(__dirname, 'dist/assets') },
 				// Copy custom DfE images
 				{ from: path.join(__dirname, 'govuk/assets'), to: path.join(__dirname, 'assets') },
-				{ from: path.join(__dirname, 'govuk/assets'), to: path.join(__dirname, 'dist/assets') },
-				// Copy rebrand favicon files to override govuk-frontend favicons (must be after govuk-frontend copy)
-				{ from: path.join(__dirname, 'assets/rebrand/images/favicon.ico'), to: path.join(__dirname, 'dist/assets/images/favicon.ico') },
-				{ from: path.join(__dirname, 'assets/rebrand/images/favicon.svg'), to: path.join(__dirname, 'dist/assets/images/favicon.svg') },
-				{ from: path.join(__dirname, 'assets/rebrand/images/govuk-icon-mask.svg'), to: path.join(__dirname, 'dist/assets/images/govuk-icon-mask.svg') },
-				{ from: path.join(__dirname, 'assets/rebrand/images/govuk-icon-180.png'), to: path.join(__dirname, 'dist/assets/images/govuk-icon-180.png') },
-				{ from: path.join(__dirname, 'assets/rebrand/images/govuk-icon-192.png'), to: path.join(__dirname, 'dist/assets/images/govuk-icon-192.png') },
-				{ from: path.join(__dirname, 'assets/rebrand/images/govuk-icon-512.png'), to: path.join(__dirname, 'dist/assets/images/govuk-icon-512.png') },
-				{ from: path.join(__dirname, 'assets/rebrand/manifest.json'), to: path.join(__dirname, 'dist/assets/images/manifest.json') },
+				{ from: path.join(__dirname, 'govuk/assets'), to: path.join(__dirname, 'dist/assets') }
+
 			],
 		})
 	],
