19d24dffe033af79a08bf86133118f82a1560584
Ali <Muhammad.ALI-SHAH@education.gov.uk>
2026-05-31 19:21:33 +0100

    Add migration

---
+++ src/Core/Dfe.Complete.Infrastructure/Migrations/20260529175951_AddCommercialTransferAgreementTask.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.Complete.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCommercialTransferAgreementTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "transfer_agreement_unamended",
+                schema: "complete",
+                table: "transfer_tasks_data",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "transfer_agreement_unamended",
+                schema: "complete",
+                table: "conversion_tasks_data",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "transfer_agreement_unamended",
+                schema: "complete",
+                table: "transfer_tasks_data");
+
+            migrationBuilder.DropColumn(
+                name: "transfer_agreement_unamended",
+                schema: "complete",
+                table: "conversion_tasks_data");
+        }
+    }
+}
