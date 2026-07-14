ee8c60b2e450230b24de67dcb13ad72c562c6199
Tristan Collen <tristan.collen@nimbleapproach.com>
2026-07-14 16:10:47 +0100

    Add ability to make option exclusive

--- src/Frontend/Dfe.Complete/Pages/Projects/TaskList/Tasks/AccuracyOfHigherNeedsTask/AccuracyOfHigherNeedsTask.cshtml.cs
+++ src/Frontend/Dfe.Complete/Pages/Projects/TaskList/Tasks/AccuracyOfHigherNeedsTask/AccuracyOfHigherNeedsTask.cshtml.cs
@@ -12,6 +12,14 @@ namespace Dfe.Complete.Pages.Projects.TaskList.Tasks.AccuracyOfHigherNeedsTask
     public class AccuracyOfHigherNeedsTaskModel(ISender sender, IAuthorizationService authorizationService, ILogger<AccuracyOfHigherNeedsTaskModel> logger, IProjectPermissionService projectPermissionService)
     : BaseProjectTaskModel(sender, authorizationService, logger, NoteTaskIdentifier.CheckAccuracyOfHigherNeeds, projectPermissionService)
     {
+        private const string OptionConfirmPublishedNumber = "confirm-published-number";
+        private const string OptionConfirmNumber = "confirm-number";
+        private const string OptionCheckReturnedForm = "check-returned-form";
+        private const string OptionSendForm = "send-form";
+
+        [BindProperty]
+        public List<string> SelectedOptions { get; set; } = [];
+
         [BindProperty]
         public bool? ConfirmNumber { get; set; }
 
@@ -46,10 +54,28 @@ namespace Dfe.Complete.Pages.Projects.TaskList.Tasks.AccuracyOfHigherNeedsTask
             AcknowledgeLAMustConfirm = ConversionTaskData.CheckAccuracyOfHigherNeedsAcknowledgeLAMustConfirm;
             CheckReturnedForm = ConversionTaskData.CheckAccuracyOfHigherNeedsCheckReturnedForm;
             SendForm = ConversionTaskData.CheckAccuracyOfHigherNeedsSendForm;
+
+            SelectedOptions = [];
+            if (ConfirmPublishedNumber == true)
+                SelectedOptions.Add(OptionConfirmPublishedNumber);
+            if (ConfirmNumber == true)
+                SelectedOptions.Add(OptionConfirmNumber);
+            if (CheckReturnedForm == true)
+                SelectedOptions.Add(OptionCheckReturnedForm);
+            if (SendForm == true)
+                SelectedOptions.Add(OptionSendForm);
+
             return Page();
         }
         public async Task<IActionResult> OnPost()
         {
+            SelectedOptions ??= [];
+
+            ConfirmPublishedNumber = SelectedOptions.Contains(OptionConfirmPublishedNumber);
+            ConfirmNumber = SelectedOptions.Contains(OptionConfirmNumber);
+            CheckReturnedForm = SelectedOptions.Contains(OptionCheckReturnedForm);
+            SendForm = SelectedOptions.Contains(OptionSendForm);
+
             await Sender.Send(new UpdateAccuracyOfHigherNeedsTaskCommand(new TaskDataId(
                 TasksDataId.GetValueOrDefault())!,
                 ConfirmNumber,
